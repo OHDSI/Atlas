d5fa45f45fc6e90423c752f09dbfe402887883d0
Anthony Sena <asena5@its.jnj.com>
2016-02-26 14:35:43 -0500

    Unification of the concept set browser

--- js/components/conceptset-browser.js
+++ js/components/conceptset-browser.js
@@ -3,26 +3,24 @@ define(['knockout', 'text!./conceptset-browser.html', 'bootstrap', 'cohortconcep
 		var self = this;
 		self.model = params.model;
 
-		self.onAtlasConceptSetSelectAction = function (result) {
-			alert(result);
-		}
-
 		self.onRespositoryConceptSetSelected = function (conceptSet) {
 			window.location.href = "#/conceptset/" + conceptSet.id + '/details';
 		}
+        
+        self.onConceptSetBrowserAction = function (result) {
+            // Inspect the result to see what type of action was taken. For now
+            // we're handling the 'add' action
+            if (result.action == 'add') {
+                self.newConceptSet();
+            }
+        }
 
 		self.newConceptSet = function () {
 			if (pageModel.currentConceptSet() == undefined) {
-				/*
-				pageModel.currentConceptSet({
-					name: 'New Concept Set',
-					id: 0
-				});
-				*/
 				pageModel.currentConceptSetSource('repository');
 				document.location = '#/conceptset/0/details';
 			}
-		}
+		}        
 	}
 
 	var component = {
