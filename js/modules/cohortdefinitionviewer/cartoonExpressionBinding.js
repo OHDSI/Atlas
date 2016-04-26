5a8bd22c4b2aec5c715a91affcc78400e3ca28aa
Sigfried Gold <sigfried@sigfried.org>
2016-04-26 13:37:56 -0400

    arrow heads on ends of additional criteria lines

--- js/modules/cohortdefinitionviewer/cartoonExpressionBinding.js
+++ js/modules/cohortdefinitionviewer/cartoonExpressionBinding.js
@@ -3,6 +3,7 @@ define(['knockout','d3'], function (ko, d3) {
   window.d3 = d3;
   var width = 300;
   var height = 150;
+  var lineHeight = 14;
 
   ko.bindingHandlers.cartoonExpression = {
     init: function (element, valueAccessor, allBindingsAccessor) {
@@ -31,18 +32,48 @@ define(['knockout','d3'], function (ko, d3) {
           .attr('orient', 'auto')
           .append('path')
             .attr('d', 'M 10 0 L 10 10 L 0 5 z')
+      svg.append('marker')
+          .attr('id', 'line-stop')
+          .attr('viewBox', '0 0 2 10')
+          .attr('refX', 0)
+          .attr('refY', 5)
+          .attr('markerUnits', 'strokeWidth')
+          .attr('markerWidth', 0.5)
+          .attr('markerHeight', 2)
+          .attr('orient', 'auto')
+          .append('path')
+            .attr('d', 'M 0 0 L 1 0 L 1 10 L 0 10 z')
     },
     update: function (element, valueAccessor, allBindingsAccessor) {
       var expression = valueAccessor()[0];
       var selectedFragment = valueAccessor()[1];
-      console.log(selectedFragment());
+
+      var criteriaGroup = expression().AdditionalCriteria();
+      var domain = {
+        min: -expression().PrimaryCriteria().ObservationWindow.PriorDays(), 
+        max: expression().PrimaryCriteria().ObservationWindow.PostDays()
+      };
+      var primaryWindow = { min: domain.min, max: domain.max };
+      var getEndPoints = function(cg) {
+        var s = cg.StartWindow.Start.Days() *
+                cg.StartWindow.Start.Coeff();
+        var e = cg.StartWindow.End.Days() *
+                cg.StartWindow.End.Coeff();
+        domain.min = Math.min(domain.min, s);
+        domain.max = Math.max(domain.max, e);
+      };
+      criteriaGroupWalk(criteriaGroup, getEndPoints);
+      var scale = d3.scale.linear()
+                    .domain([domain.min, domain.max])
+                    .range([0.05 * width, 0.9 * width]);
+
       var svg = d3.select(element).select('svg');
       //svg.select('g.primary').remove();
       svg.selectAll('g').remove();
       var g =svg.append('g')
                 .attr('class','primary')
                 .attr('transform', 
-                      'translate(' + (width/2) + ',10)')
+                      'translate(0,' + lineHeight + ')')
               .on('mouseover', function() {
                 d3.select("#primary-criteria")
                   .style('background-color', 'pink')
@@ -65,8 +96,10 @@ define(['knockout','d3'], function (ko, d3) {
           .style('opacity',.3)
           .attr('id', 'cartoon-primary-criteria')
       g.append('line')
-          .attr('x1', -50)
-          .attr('x2', 100)
+          .attr('x1', scale(-expression().PrimaryCriteria()
+                        .ObservationWindow.PriorDays())) 
+          .attr('x2', scale(expression().PrimaryCriteria()
+                        .ObservationWindow.PostDays()))
           .attr('y1', 0)
           .attr('y2', 0)
           .attr('stroke-width', 4)
@@ -76,29 +109,12 @@ define(['knockout','d3'], function (ko, d3) {
           .style('marker-end', 'url(#right-arrow)')
       g.append('circle')
           .attr('r', 8)
-          .attr('cx', 0)
+          .attr('cx', scale(0))
           .attr('cy', 0)
           .style('fill','brown')
           .style('stroke','black')
 
-      var criteriaGroup = expression().AdditionalCriteria();
-      var domain = {
-        min: -expression().PrimaryCriteria().ObservationWindow.PriorDays(), 
-        max: expression().PrimaryCriteria().ObservationWindow.PostDays()
-      };
-      var getEndPoints = function(cg) {
-        var s = cg.StartWindow.Start.Days() *
-                cg.StartWindow.Start.Coeff();
-        var e = cg.StartWindow.End.Days() *
-                cg.StartWindow.End.Coeff();
-        domain.min = Math.min(domain.min, s);
-        domain.max = Math.max(domain.max, e);
-      };
-      criteriaGroupWalk(criteriaGroup, getEndPoints);
-      var scale = d3.scale.linear()
-                    .domain([domain.min, domain.max])
-                    .range([-width/2, width/2]);
-      drawCartoon(g, criteriaGroup, 2, scale, selectedFragment)
+      drawCartoon(g, criteriaGroup, 2, scale, selectedFragment, primaryWindow)
     }
   };
   function criteriaGroupWalk(criteriaGroup, cb) {
@@ -111,43 +127,69 @@ define(['knockout','d3'], function (ko, d3) {
       criteriaGroupWalk(groups[i], cb);
     }
   }
-  function drawCartoon(selection, data, linesdown, scale, selectedFragment) {
-      //console.log("cartoon update", ko.toJS(valueAccessor), ko.toJS(allBindingsAccessor));
-      console.log("draw", linesdown);
+  function drawCartoon(selection, data, linesdown, scale, selectedFragment, primaryWindow) {
       var morelines = 0;
       var g = selection
               .append('g')
                 .attr('class','additional')
                 .attr('transform', 'translate(0,' + 
-                      (linesdown*10) +')')
+                      (linesdown*lineHeight) +')')
       g.selectAll('g.additional')
               .data(data.CriteriaList())
                 .enter()
               .append('line')
+                .style('marker-start', function(cg) {
+                  var days = cg.StartWindow.Start.Days();
+                  if (days === null) {
+                    return 'url(#left-arrow)';
+                  }
+                  return 'url(#line-stop)';
+                })
+                .style('marker-end', function(cg) {
+                  var days = cg.StartWindow.End.Days();
+                  if (days === null) {
+                    return 'url(#right-arrow)';
+                  }
+                  return 'url(#line-stop)';
+                })
                 .attr('x1', function(cg) {
-                  return cg.StartWindow.Start.Days() *
-                         cg.StartWindow.Start.Coeff();
+                  var days = cg.StartWindow.Start.Days();
+                  var coeff = cg.StartWindow.Start.Coeff();
+                  if (days === null) {
+                    if (coeff < 0)
+                      return scale(primaryWindow.min);
+                    else
+                      return scale(primaryWindow.max);
+                  }
+                  return scale(days * coeff);
                 })
                 .attr('x2', function(cg) {
-                  return cg.StartWindow.End.Days() *
-                         cg.StartWindow.End.Coeff();
+                  var days = cg.StartWindow.End.Days();
+                  var coeff = cg.StartWindow.End.Coeff();
+                  if (days === null) {
+                    if (coeff < 0)
+                      return scale(primaryWindow.min);
+                    else
+                      return scale(primaryWindow.max);
+                  }
+                  return scale(days * coeff);
                 })
-                .attr('y1', function(d,i) { return i*10;})
-                .attr('y2', function(d,i) { return i*10;})
+                .attr('y1', function(d,i) { return i*lineHeight;})
+                .attr('y2', function(d,i) { return i*lineHeight;})
                 .attr('stroke-width', 4)
-                //.attr('stroke', 'blue')
-                .attr('stroke', function(d) {
-                  return d === selectedFragment() ?
-                      'red' : 'blue';
+                .classed('highlighted', function(d) {
+                  return d === selectedFragment();
                 })
+                //.attr('stroke', 'blue')
+                //.attr('stroke', function(d) {
+                  //return d === selectedFragment() ?  'red' : 'blue';
+                //})
                 .on('mouseover', function(d) {
                   selectedFragment(d);
                 })
-      console.log(g[0]);
       var groups = data.Groups();
       for (var i = 0; i < groups.length; i++) {
-        console.log('subgroup');
-        drawCartoon(g, groups[i], data.CriteriaList().length, scale, selectedFragment);
+        drawCartoon(g, groups[i], data.CriteriaList().length, scale, selectedFragment, primaryWindow);
       }
   }
 });
