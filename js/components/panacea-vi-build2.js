295a8e6efda31fc725e4f0e9467e30118a362043
cwen <cwen@192.168.1.122>
2016-08-02 12:01:20 -0400

    OHDSI-75 => fix a NPE

--- js/components/panacea-vi-build2.js
+++ js/components/panacea-vi-build2.js
@@ -1211,8 +1211,13 @@
 			str = str.toLowerCase().split(' ');      
 
 			for(var i = 0; i < str.length; i++){     
-			  str[i] = str[i].split('');         
-			  str[i][0] = str[i][0].toUpperCase();
+			  str[i] = str[i].split('');
+			  //Chen -- fix NPE
+			  if(str[i][0] != null){
+				  str[i][0] = str[i][0].toUpperCase();
+			  } else{
+				  str[i][0] = '';
+			  }
 			  str[i] = str[i].join('');           
 			}
 			return str.join(' '); 
