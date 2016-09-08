5bb4946e419ef535b4a0cacfd9f29b8728f9398e
Sigfried Gold <sigfried@sigfried.org>
2016-09-08 15:01:47 -0400

    styling improvements

--- js/modules/conceptpicker/ConceptPicker.js
+++ js/modules/conceptpicker/ConceptPicker.js
@@ -1,4 +1,4 @@
-define(['jquery','knockout', 'text!./ConceptPickerTemplate.html', './InputTypes/Concept', 'vocabularyprovider', 'knockout-jqueryui/dialog', 'css!./css/conceptpicker.css'], function ($, ko, template, Concept, VocabularyProvider) {
+define(['jquery','knockout', 'text!./ConceptPickerTemplate.html', './InputTypes/Concept', 'vocabularyprovider', 'knockout-jqueryui/dialog'], function ($, ko, template, Concept, VocabularyProvider) {
 	
 	function _mapConceptRowToConcept (row)
 	{
