3d5f2064c123b6a4f0f2e93dfd940a836e319105
Chris Knoll <cknoll@ohdsi.org>
2016-12-21 10:46:00 -0500

    Added UI elements to support censoring criteria.

--- js/modules/cohortbuilder/components/CohortExpressionEditor.js
+++ js/modules/cohortbuilder/components/CohortExpressionEditor.js
@@ -139,6 +139,129 @@ define(['knockout', '../options', '../CriteriaGroup', '../CriteriaTypes', '../Co
 			}
 		];
 
+		var censorCriteriaOptions = [
+			{
+				text: "Add Condition Era Criteria",
+				selected: false,
+				description: "Exit cohort based on diagosis era.",
+				action: function () {
+					self.expression().PrimaryCriteria().CriteriaList.push({
+						ConditionEra: new criteriaTypes.ConditionEra(null, self.expression().ConceptSets)
+					});
+				}
+			},
+			{
+				text: "Add Condition Occurrence Criteria",
+				selected: false,
+				description: "Exit cohort based on  diagnoses.",
+				action: function () {
+					self.expression().CensoringCriteria.push({
+						ConditionOccurrence: new criteriaTypes.ConditionOccurrence(null, self.expression().ConceptSets)
+					});
+				}
+			},
+			{
+				text: "Add Death Criteria",
+				selected: false,
+				description: "Exit cohort based on  death.",
+				action: function () {
+					self.expression().CensoringCriteria.push({
+						Death: new criteriaTypes.Death(null, self.expression().ConceptSets)
+					});
+				}
+			},
+			{
+				text: "Add Device Exposure Criteria",
+				selected: false,
+				description: "Exit cohort based on  device exposure.",
+				action: function () {
+					self.expression().CensoringCriteria.push({
+						DeviceExposure: new criteriaTypes.DeviceExposure(null, self.expression().ConceptSets)
+					});
+				}
+			},
+			{
+				text: "Add Dose Era Criteria",
+				selected: false,
+				description: "Exit cohort based on dose eras.",
+				action: function () {
+					self.expression().CensoringCriteria.push({
+						DoseEra: new criteriaTypes.DoseEra(null, self.expression().ConceptSets)
+					});
+				}
+			},
+			{
+				text: "Add Drug Era Criteria",
+				selected: false,
+				description: "Exit cohort based on drugs over time.",
+				action: function () {
+					self.expression().CensoringCriteria.push({
+						DrugEra: new criteriaTypes.DrugEra(null, self.expression().ConceptSets)
+					});
+				}
+			},
+			{
+				text: "Add Drug Exposure Criteria",
+				selected: false,
+				description: "Exit cohort based on exposure to specific drugs or drug classes.",
+				action: function () {
+					self.expression().CensoringCriteria.push({
+						DrugExposure: new criteriaTypes.DrugExposure(null, self.expression().ConceptSets)
+					});
+				}
+			},
+			{
+				text: "Add Measurement Criteria",
+				selected: false,
+				description: "Exit cohort based on Measurement.",
+				action: function () {
+					self.expression().CensoringCriteria.push({
+						Measurement: new criteriaTypes.Measurement(null, self.expression().ConceptSets)
+					});
+				}
+			},
+			{
+				text: "Add Observation Criteria",
+				selected: false,
+				description: "Exit cohort based on lab tests or other observations.",
+				action: function () {
+					self.expression().CensoringCriteria.push({
+						Observation: new criteriaTypes.Observation(null, self.expression().ConceptSets)
+					});
+				}
+			},
+			{
+				text: "Add Procedure Occurrence Criteria",
+				selected: false,
+				description: "Exit cohort based on procedures.",
+				action: function () {
+					self.expression().CensoringCriteria.push({
+						ProcedureOccurrence: new criteriaTypes.ProcedureOccurrence(null, self.expression().ConceptSets)
+					});
+				}
+			},
+			{
+				text: "Add Specimen Criteria",
+				selected: false,
+				description: "Find patients based on Specimen.",
+				action: function () {
+					self.expression().CensoringCriteria.push({
+						Specimen: new criteriaTypes.Specimen(null, self.expression().ConceptSets)
+					});
+				}
+			},
+			{
+				text: "Add Visit Criteria",
+				selected: false,
+				description: "Exit cohort based on visit information.",
+				action: function () {
+					self.expression().CensoringCriteria.push({
+						VisitOccurrence: new criteriaTypes.VisitOccurrence(null, self.expression().ConceptSets)
+					});
+				}
+			}
+		];
+
 		if (params.widget) {
 			params.widget(this);
 		}
@@ -165,6 +288,10 @@ define(['knockout', '../options', '../CriteriaGroup', '../CriteriaTypes', '../Co
 
 		self.removePrimaryCriteria = function (criteria) {
 			self.expression().PrimaryCriteria().CriteriaList.remove(criteria);
+		}
+		
+		self.removeCensoringCriteria = function (criteria) {
+			self.expression().CensoringCriteria.remove(criteria);	
 		}
 
 		self.addInclusionRule = function () {
@@ -197,6 +324,17 @@ define(['knockout', '../options', '../CriteriaGroup', '../CriteriaTypes', '../Co
 			}
 		};
 
+		self.addCensorCriteriaOptions = {
+			selectText: "Add Censor Event...",
+			width: 250,
+			height: 300,
+			actionOptions: censorCriteriaOptions,
+			onAction: function (data) {
+				data.selectedData.action();
+			}
+		};
+
+		
 		self.getCriteriaIndexComponent = function (data) {
 			data = ko.utils.unwrapObservable(data);
 
