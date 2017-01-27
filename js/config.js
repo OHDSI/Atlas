2fabcdd77269141b7ec0cb91e306a14e6d731052
Mark Velez <mav7014@nyp.org>
2017-01-27 10:35:24 -0500

    Remove support for legacy (json-based) data-sources reports
     * Incompatible with Atlas v2 libraries
     * Note: removes obsolete lines from config.js and main.js

--- js/config.js
+++ js/config.js
@@ -20,8 +20,6 @@ define([], function () {
 	config.webAPIRoot = config.services[0].url;
 	// config.rServicesHost = 'http://hixbeta.jnj.com:8999/';
 	// config.rServicesHost = 'http://rndusrdhit09.jnj.com:8080/';
-	config.dataSourcesLocation = '/achilles/data/datasources.json';
-	config.dataSourcesRoot = '/achilles/data';
     config.cohortComparisonResultsEnabled = false;
 	config.userAuthenticationEnabled = false;
 
