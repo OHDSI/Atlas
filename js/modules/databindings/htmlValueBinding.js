a05903b6cea8fd1163057304a2c6c6ee275a898b
Chris Knoll <cknoll@ohdsi.org>
2017-03-22 16:46:12 -0400

    Added fix to htmlValueBinding to work in latest Chrome (57.0).

--- js/modules/databindings/htmlValueBinding.js
+++ js/modules/databindings/htmlValueBinding.js
@@ -17,6 +17,9 @@ define(['knockout'], function (ko) {
 		update: function (element, valueAccessor, allBindingsAccessor) {
 			var value = ko.utils.unwrapObservable(valueAccessor());
 			var allBindings = allBindingsAccessor();
+			
+			if (element === document.activeElement) return; // element is active, so don't overwrite the html value
+			
 			if ((value == null || value.length == 0) && allBindings.defaultValue)
 				element.innerHTML = allBindings.defaultValue;
 			else
