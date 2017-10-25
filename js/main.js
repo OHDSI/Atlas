93ea95ae6dab68a1bc0d0e89a59e7e0f04bfff45
Anthony Sena <asena5@its.jnj.com>
2017-10-25 15:30:20 -0400

    Remove nvd3 references

--- js/main.js
+++ js/main.js
@@ -108,7 +108,6 @@ requirejs.config({
 		"cohortcomparison": "modules/cohortcomparison",
 		"r-manager": "components/r-manager",
 		"negative-controls": "components/negative-controls",
-		//"nvd3": "nv.d3",
 		"atlascharts": "https://unpkg.com/@ohdsi/atlascharts@1.1.0/dist/atlascharts.min",
 		"jnj_chart": "jnj.chart", // scatterplot is not ported to separate library
 		"lodash": "lodash.4.15.0.full",
