e37abbe57508e40854568f3b5fb0ff5e1d9f74c1
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2018-03-26 10:46:03 +0300

    Show dates for all entities

--- js/components/cohort-comparison-browser.js
+++ js/components/cohort-comparison-browser.js
@@ -1,4 +1,4 @@
-define(['knockout', 'text!./cohort-comparison-browser.html', 'appConfig', 'webapi/MomentAPI', 'webapi/AuthAPI', 'cohortcomparison/ComparativeCohortAnalysis','faceted-datatable','access-denied'], function (ko, view, config, momentApi, authApi) {
+define(['knockout', 'text!./cohort-comparison-browser.html', 'appConfig', 'webapi/MomentAPI', 'webapi/AuthAPI', 'cohortcomparison/ComparativeCohortAnalysis','faceted-datatable'], function (ko, view, config, momentApi, authApi) {
 	function cohortComparisonBrowser(params) {
 		var self = this;
 		self.reference = ko.observableArray();
