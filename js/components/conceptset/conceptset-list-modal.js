d3d32dcbacc3b65cdaf19781ffec40ea35e97cdb
Anthony Sena <asena5@its.jnj.com>
2018-04-20 20:08:02 -0400

    Adding a title bar to the concept set modal

--- js/components/conceptset/conceptset-list-modal.js
+++ js/components/conceptset/conceptset-list-modal.js
@@ -3,6 +3,7 @@ define(['knockout','text!./conceptset-list-modal.html'], function (ko, template)
 	function ConceptSetListModal(params) {
 		const self = this;
 		self.model = params.model;
+		self.title = params.title || 'Concept List';
 		self.conceptSetList = params.conceptSetList;
 		self.isShown = params.isShown;
 	}
