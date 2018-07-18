6278ebaa5d49004f9aa0d32c21db177ca56b9cc7
Anton Gackovka <anton.gackovka@odysseusinc.com>
2018-07-18 11:48:07 +0300

    ATLP-443. Reverted 'Included' word

--- js/components/role-details.js
+++ js/components/role-details.js
@@ -56,7 +56,7 @@ define(['knockout', 'text!./role-details.html', 'appConfig', 'ohdsi.util', 'data
         self.selectAllUsers = () => self.userItems().forEach(user => user.isRoleUser(true));
         self.deselectAllUsers = () => self.userItems().forEach(user => user.isRoleUser(false));
         
-        self.checkboxTitle = '<span class="fa fa-check" data-bind="css: { selected: $component.areUsersSelected }, click: function(){$parent.areUsersSelected()?$parent.deselectAllUsers():$parent.selectAllUsers();}"></span>';
+        self.checkboxTitle = '<span class="fa fa-check" data-bind="css: { selected: $component.areUsersSelected }, click: function(){$parent.areUsersSelected()?$parent.deselectAllUsers():$parent.selectAllUsers();}"></span> Included';
         
         self.renderCheckbox = function (field, editable) {
             return editable
