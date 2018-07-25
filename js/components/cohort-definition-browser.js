2452ca91df356564613e13a3875f1f383a3f5f51
Alex Saltykov <johnSamilin@users.noreply.github.com>
2018-07-25 20:50:21 +0200

    Refactored cohort-definitions page (#780)
    moved all 3rd-party libs to assets folder
    moved cohort-definition-manager to pages folder
    used @odsi/ui-toolbox package
    moved modules cohortcomparison, cohortbuilder to components
    moved cohortfeatures from modules to pages/cohort-definitions
    moved cost & utilization components to a separate folder
    moved cohort-specific APIs to services folder

---
+++ js/components/cohort-definition-browser.js
@@ -0,0 +1,105 @@
+define([
+	'knockout',
+	'text!./cohort-definition-browser.html',
+	'appConfig',
+	'webapi/AuthAPI',
+	'webapi/MomentAPI',
+	'providers/Component',
+	'utils/commonUtils',
+	'services/http',
+	'faceted-datatable',
+], function (
+	ko,
+	view,
+	config,
+	authApi,
+	momentApi,
+	Component,
+	commonUtils,
+	httpService
+) {
+	class CohortDefinitionBrowser extends Component {
+		constructor(params) {
+			super(params);
+			this.reference = ko.observableArray();
+			this.selected = params.cohortDefinitionSelected;
+			this.loading = ko.observable(false);
+			this.config = config;
+
+			this.loading(true);
+
+			httpService.doGet(`${config.api.url}cohortdefinition`)
+				.then(({ data }) => this.reference(data))
+				.finally(() => { this.loading(false) });
+
+
+			this.options = {
+				Facets: [{
+						'caption': 'Last Modified',
+						'binding': function (o) {
+							var createDate = new Date(o.createdDate);
+							var modDate = new Date(o.modifiedDate);
+							var dateForCompare = (createDate > modDate) ? createDate : modDate;
+							var daysSinceModification = (new Date()
+								.getTime() - dateForCompare.getTime()) / 1000 / 60 / 60 / 24;
+							if (daysSinceModification < 7) {
+								return 'This Week';
+							} else if (daysSinceModification < 14) {
+								return 'Last Week';
+							} else {
+								return '2+ Weeks Ago';
+							}
+						}
+					},
+					{
+						'caption': 'Author',
+						'binding': function (o) {
+							return o.createdBy;
+						}
+					}
+				]
+			};
+
+			this.columns = [{
+					title: 'Id',
+					data: 'id'
+				},
+				{
+					title: 'Name',
+					render: this.renderCohortDefinitionLink
+				},
+				{
+					title: 'Created',
+					type: 'date',
+					render: function (s, p, d) {
+						return momentApi.formatDateTimeUTC(d.createdDate);
+					}
+				},
+				{
+					title: 'Updated',
+					type: 'date',
+					render: function (s, p, d) {
+						return momentApi.formatDateTimeUTC(d.modifiedDate);
+					}
+				},
+				{
+					title: 'Author',
+					data: 'createdBy'
+				}
+			];
+
+			this.renderCohortDefinitionLink = this.renderCohortDefinitionLink.bind(this);
+			this.rowClick = this.rowClick.bind(this);
+		}
+		
+		renderCohortDefinitionLink (s, p, d) {
+			return '<span class="linkish">' + d.name + '</span>';
+		}
+
+		rowClick (d) {
+			this.selected(d.id);
+		}
+	}
+
+	return commonUtils.build('cohort-definition-browser', CohortDefinitionBrowser, view);
+});
---
+++ js/pages/cohort-definitions/components/reporting/cost-utilization/drug-util-summary.js
@@ -0,0 +1,70 @@
+define(
+  [
+    'knockout',
+    'text!./drug-util-summary.html',
+    './base-drug-util-report',
+    'services/CohortResultsService',
+    'utils/commonUtils',
+    'components/visualizations/filter-panel/filter-panel',
+    'less!./drug-util-summary.less',
+  ],
+  function (
+    ko,
+    view,
+    BaseDrugUtilReport,
+    CohortResultsService,
+    commonUtils
+  ) {
+
+    const componentName = 'cost-utilization-drug-summary-util';
+
+    class DrugUtilSummaryReport extends BaseDrugUtilReport {
+
+      constructor(params) {
+        super(params);
+
+        this.onDrugSelect = params.onDrugSelect;
+
+        this.drugsTableColumns = [
+          {
+            title: 'OMOP Concept',
+            data: 'drugId',
+            className: this.classes('tbl-col', 'drug-id'),
+            render: id => `<a href=\"#/concept/${id}\">${id}</a>`,
+          },
+          {
+            title: 'Drug',
+            data: 'drugName',
+            className: this.classes('tbl-col', 'drug'),
+          },
+          {
+            title: 'Concept vocabulary',
+            data: 'drugVocabularyId',
+            className: this.classes('tbl-col', 'drug-vocab'),
+          },
+          {
+            title: 'Concept class',
+            data: 'drugClass',
+            className: this.classes('tbl-col', 'drug-class'),
+          },
+          ...this.drugsTableColumns,
+        ];
+
+        this.init();
+        this.loadFilterOptions();
+      }
+
+      fetchAPI({ filters }) {
+        return CohortResultsService.loadDrugUtilSummaryReport({
+            source: this.source,
+            cohortId: this.cohortId,
+            window: this.window,
+            filters,
+          })
+          .then(({ data }) => this.dataList(data));
+      }
+    }
+
+    return commonUtils.build(componentName, DrugUtilSummaryReport, view);
+  }
+);
