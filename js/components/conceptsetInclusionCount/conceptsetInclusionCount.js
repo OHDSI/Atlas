4e11e7c1bd42087fa00918a14f3308922b281c37
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2018-08-16 20:35:35 +0300

    Added Lazy loading inclusion concept counts (#841)

---
+++ js/components/conceptsetInclusionCount/conceptsetInclusionCount.js
@@ -0,0 +1,39 @@
+define([
+	'knockout',
+	'text!./conceptsetInclusionCount.html',
+	'providers/Component',
+	'utils/CommonUtils',
+	'services/ConceptSet',
+], function(
+	ko,
+	view,
+	Component,
+	commonUtils,
+	conceptSetApi,
+){
+
+	class ConceptSetInclusionCount extends Component{
+
+		constructor(params) {
+			super(params);
+			this.countLoading = ko.observable();
+			this.inclusionCount = ko.observable(0);
+
+			this.conceptSetExpression = params.conceptSetExpression || ko.observableArray();
+
+			this.getInclusionCount = this.getInclusionCount.bind(this);
+			this.conceptSetExpression.subscribe(this.getInclusionCount);
+			this.getInclusionCount();
+		}
+
+		getInclusionCount() {
+			this.countLoading(true);
+			conceptSetApi.getInclusionCount(this.conceptSetExpression())
+				.then(({data}) => this.inclusionCount(data))
+				.finally(() => this.countLoading(false));
+		}
+
+	}
+
+	commonUtils.build('conceptset-inclusion-count', ConceptSetInclusionCount, view);
+});
