67d6ea29d1cf92cc0aef69fc076d0add39533618
Alex Saltykov <johnSamilin@users.noreply.github.com>
2018-09-12 17:21:14 +0300

    Delete local.config.js

--- js/config/local.config.js
+++
@@ -1,9 +0,0 @@
-define([], function () {
-	var config = {};
-	config.userAuthenticationEnabled = true;
-  config.useBundled3dPartyLibs = false;
-	config.cohortComparisonResultsEnabled = true;
-	config.useExecutionEngine = true;
-
-	return config;
-});
