2fa8578593639c3577d3a56745d03ce53449b530
ASaltykov <alexander.saltykov@odysseusinc.com>
2018-09-19 13:42:06 +0300

    no message

---
+++ js/pages/home/home.js
@@ -0,0 +1,49 @@
+define([
+	'knockout',
+	'text!./home.html',
+	'providers/Page',
+	'utils/CommonUtils',
+	'services/http',
+	'appConfig',
+	'webapi/AuthAPI',
+	'components/heading',
+], function (
+	ko,
+	view,
+	Page,
+	commonUtils,
+	httpService,
+	config,
+	authApi
+) {
+	class Home extends Page {
+		constructor(params) {
+			super(params);
+			this.github_status = ko.observableArray();
+			
+			this.canCreateCohort = ko.pureComputed(() => {
+				return (authApi.isAuthenticated() && authApi.isPermittedCreateCohort()) || !config.userAuthenticationEnabled;
+			});
+
+			this.canSearch = ko.computed(() => {
+				return authApi.isAuthenticated();
+			});
+		}
+
+		async onPageCreated() {
+			const { data } = await httpService.doGet("https://api.github.com/repos/OHDSI/Atlas/issues?state=closed&milestone=18");
+			this.github_status(data);
+		}
+
+		newCohortDefinition() {
+			document.location = "#/cohortdefinition/0";
+		}
+
+		browseVocabulary() {
+			document.location = "#/search";
+		}
+
+	}
+
+	return commonUtils.build('home', Home, view);
+});
