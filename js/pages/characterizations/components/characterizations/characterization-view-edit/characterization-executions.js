6dc757c6d470fdc4418d22e8cf7c7d8d44b1bf6b
Pavel Grafkin <pavgra@gmail.com>
2018-09-20 16:53:44 -0400

    Fixed "view results" link for failed

--- js/pages/characterizations/components/characterizations/characterization-view-edit/characterization-executions.js
+++ js/pages/characterizations/components/characterizations/characterization-view-edit/characterization-executions.js
@@ -83,7 +83,7 @@ define([
                         data: 'results',
                         className: this.classes('col-exec-results'),
                         render: (s, p, d) => {
-                            return d.status === this.ccGenerationStatusOptions.STARTED ? '-' : `<a data-bind="css: $component.classes('reports-link'), click: $component.goToResults.bind(null, id)">View reports</a>`; // ${d.reportCount}
+                            return d.status === this.ccGenerationStatusOptions.COMPLETED ? `<a data-bind="css: $component.classes('reports-link'), click: $component.goToResults.bind(null, id)">View reports</a>` : '-'; // ${d.reportCount}
                         }
                     }
                 ];
