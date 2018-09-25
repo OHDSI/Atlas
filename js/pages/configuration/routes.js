7b2a68732313e8831b04adf1eb4bf75aec1f546c
mpozhidaeva <31308645+mpozhidaeva@users.noreply.github.com>
2018-09-25 15:42:19 +0300

    Fixed empty new source page (#936)

--- js/pages/configuration/routes.js
+++ js/pages/configuration/routes.js
@@ -34,9 +34,7 @@ define(
         '/source/:id': new AuthorizedRoute((id) => {
           appModel.activePage(this.title);
           require(['./sources/source-manager'], function () {
-            if (id !== 'new') {
-              appModel.selectedSourceId(id);
-            }
+            appModel.selectedSourceId(id !== 'new' ? id : null);
             appModel.currentView('source-manager');
           });
         }),
