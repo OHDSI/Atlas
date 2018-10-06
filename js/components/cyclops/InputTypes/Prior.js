de32fc8f62709041254f205578f0b7d029af7648
Anthony Sena <asena5@its.jnj.com>
2018-10-06 10:06:48 -0400

    Data type enforcement for prior/control

--- js/components/cyclops/InputTypes/Prior.js
+++ js/components/cyclops/InputTypes/Prior.js
@@ -1,12 +1,15 @@
-define(['knockout'], function (ko) {
+define([
+	'knockout', 
+	'databindings',
+], function (ko) {
 
 	function Prior(data) {
 		var self = this;
 		data = data || {};
 
         self.priorType = ko.observable(data.priorType || "laplace");
-        self.variance = ko.observable(data.variance === 0 ? 1 : data.variance || 1);
-        self.exclude = ko.observable(data.exclude === 0 ? 0 : data.exclude || 0);
+        self.variance = ko.observable(data.variance === 0 ? 1 : data.variance || 1).extend({numeric: 0});
+        self.exclude = ko.observable(data.exclude === 0 ? 0 : data.exclude || 0).extend({numeric: 0});
         self.graph = ko.observable(data.graph === 0 ? null : data.graph || null);
         self.neighborhood = ko.observable(data.neighborhood === 0 ? null : data.neighborhood || null);
         self.useCrossValidation = ko.observable(data.useCrossValidation === 0 ? false : data.useCrossValidation || false);
