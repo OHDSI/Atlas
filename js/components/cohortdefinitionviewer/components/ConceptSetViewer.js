384625fe469b7beb5ccf41439c3d26116a39c932
VAntonov <valery.antonov@firstlinesoftware.com>
2018-10-11 13:02:08 +0300

    ATLP-528 - Show All/10

--- js/components/cohortdefinitionviewer/components/ConceptSetViewer.js
+++ js/components/cohortdefinitionviewer/components/ConceptSetViewer.js
@@ -1,11 +1,21 @@
 define(['knockout','text!./ConceptSetViewerTemplate.html'], function (ko, template) {
 
+	const MAX_SIZE = 10;
+
 	function ConceptSetViewer(params) {
 		var self = this;
 		self.conceptSet = params.conceptSet;
-		
+		self.currentItems = ko.observable(params.conceptSet.expression.items.slice(0, MAX_SIZE));
+		self.showAll = ko.observable(false);
+		self.buttonText = ko.computed(() => self.showAll() ? 'Show ' + MAX_SIZE : 'Show All');
+		self.switchShowAll = () => {
+			self.showAll(!self.showAll());
+			self.currentItems(self.showAll()
+				? self.conceptSet.expression.items.slice(0)
+				: self.conceptSet.expression.items.slice(0, MAX_SIZE));
+		}
 	}
-	
+
 	// return compoonent definition
 	return {
 		viewModel: ConceptSetViewer,
