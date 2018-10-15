dc0affc00c2504600192520a30bd49b13f55b36b
Pavel Grafkin <pavgra@gmail.com>
2018-10-15 10:40:39 -0400

    Fixes for performance of Cohort - Export (text view) tab

--- js/components/cohortdefinitionviewer/components/ConceptSetViewer.js
+++ js/components/cohortdefinitionviewer/components/ConceptSetViewer.js
@@ -1,9 +1,39 @@
 define(['knockout','text!./ConceptSetViewerTemplate.html'], function (ko, template) {
 
-	function ConceptSetViewer(params) {
-		var self = this;
-		self.conceptSet = params.conceptSet;
-		
+	class ConceptSetViewer {
+		constructor(params) {
+			this.data = params.conceptSet.expression.items;
+			this.columns = [
+				{
+					title: 'Concept Id',
+					data: 'concept.CONCEPT_ID'
+				},
+				{
+					title: 'Concept Name',
+					data: 'concept.CONCEPT_NAME'
+				},
+				{
+					title: 'Domain',
+					data: 'concept.DOMAIN_ID'
+				},
+				{
+					title: 'Vocabulary',
+					data: 'concept.VOCABULARY_ID'
+				},
+				{
+					title: 'Excluded',
+					render: (s, p, d) => d.isExcluded() ? 'YES' : 'NO'
+				},
+				{
+					title: 'Descendants',
+					render: (s, p, d) => d.includeDescendants() ? 'YES' : 'NO'
+				},
+				{
+					title: 'Mapped',
+					render: (s, p, d) => d.includeMapped() ? 'YES' : 'NO'
+				}
+			];
+		}
 	}
 	
 	// return compoonent definition
