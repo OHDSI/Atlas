1b90bcfa700cb52496a4b22ded2bcdbac01b3a77
Pavel Grafkin <pavgra@gmail.com>
2018-10-15 11:25:42 -0400

    Fixed styling

--- js/components/cohortdefinitionviewer/components/ConceptSetViewer.js
+++ js/components/cohortdefinitionviewer/components/ConceptSetViewer.js
@@ -1,36 +1,53 @@
-define(['knockout','text!./ConceptSetViewerTemplate.html'], function (ko, template) {
+define([
+	'knockout',
+	'text!./ConceptSetViewerTemplate.html',
+	'providers/Component',
+	'less!./ConceptSetViewer.less'
+], function (
+	ko,
+	template,
+	Component
+) {
 
-	class ConceptSetViewer {
+	class ConceptSetViewer extends Component {
 		constructor(params) {
+			super(params);
 			this.data = params.conceptSet.expression.items;
 			this.columns = [
 				{
 					title: 'Concept Id',
-					data: 'concept.CONCEPT_ID'
+					data: 'concept.CONCEPT_ID',
+					className: this.classes('col-id')
 				},
 				{
 					title: 'Concept Name',
-					data: 'concept.CONCEPT_NAME'
+					data: 'concept.CONCEPT_NAME',
+					className: this.classes('col-name')
 				},
 				{
 					title: 'Domain',
-					data: 'concept.DOMAIN_ID'
+					data: 'concept.DOMAIN_ID',
+					className: this.classes('col-domain')
 				},
 				{
 					title: 'Vocabulary',
-					data: 'concept.VOCABULARY_ID'
+					data: 'concept.VOCABULARY_ID',
+					className: this.classes('col-vocab')
 				},
 				{
 					title: 'Excluded',
-					render: (s, p, d) => d.isExcluded() ? 'YES' : 'NO'
+					render: (s, p, d) => d.isExcluded() ? 'YES' : 'NO',
+					className: this.classes('col-excluded')
 				},
 				{
 					title: 'Descendants',
-					render: (s, p, d) => d.includeDescendants() ? 'YES' : 'NO'
+					render: (s, p, d) => d.includeDescendants() ? 'YES' : 'NO',
+					className: this.classes('col-desc')
 				},
 				{
 					title: 'Mapped',
-					render: (s, p, d) => d.includeMapped() ? 'YES' : 'NO'
+					render: (s, p, d) => d.includeMapped() ? 'YES' : 'NO',
+					className: this.classes('col-mapped')
 				}
 			];
 		}
