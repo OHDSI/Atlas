0a16a3d4acf2e049930e75f5d9d992687a2b048a
Anastasiia Klochkova <anastasiia.klochkova@odysseusinc.com>
2018-11-06 19:11:03 +0300

    moved checking for 'undefined' in right place

--- js/pages/concept-sets/concept-manager.js
+++ js/pages/concept-sets/concept-manager.js
@@ -402,7 +402,6 @@ define([
 		}
 
 		async loadConcept(conceptId) {
-			this.currentConceptArray().length = 0;
 			const { data } = await httpService.doGet(sharedState.vocabularyUrl() + 'concept/' + conceptId);
 			var exists = false;
 			for (var i = 0; i < this.model.recentConcept().length; i++) {
