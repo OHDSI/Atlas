f8a7ea475dc989082f626bca6c8ac2d01bcebc01
Anastasiia Klochkova <anastasiia.klochkova@odysseusinc.com>
2018-11-22 14:39:15 +0300

    changed visibility rules

--- js/utils/BrowserDetection.js
+++ js/utils/BrowserDetection.js
@@ -2,7 +2,7 @@
 const browser = detect.parse(navigator.userAgent);
 const isBrowserSupported = browser.browser.family.toLowerCase() === 'chrome' && parseInt(browser.browser.version) > 63;
 if (!isBrowserSupported){
-    document.getElementById("b-alarm").style.visibility = "visible";
+    document.getElementById("b-alarm").style.display = "flex";
 } else {
     hideWarning();
 }
