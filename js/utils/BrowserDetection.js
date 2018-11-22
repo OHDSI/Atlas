50ae0a3b3f587191762db5a37b05f4a46f1da78d
Anastasiia Klochkova <anastasiia.klochkova@odysseusinc.com>
2018-11-22 15:18:41 +0300

    fixed notes

--- js/utils/BrowserDetection.js
+++ js/utils/BrowserDetection.js
@@ -1,12 +1,9 @@
 
 const browser = detect.parse(navigator.userAgent);
 const isBrowserSupported = browser.browser.family.toLowerCase() === 'chrome' && parseInt(browser.browser.version) > 63;
-if (!isBrowserSupported){
-    document.getElementById("b-alarm").style.display = "flex";
-} else {
-    hideWarning();
-}
+toggleWarning(isBrowserSupported);
 
-function hideWarning() {
-    document.getElementById("b-alarm").style.display = "none";
+function toggleWarning(doHide) {
+
+    document.getElementById("b-alarm").style.display = (doHide ? "none" : "flex");
 }
