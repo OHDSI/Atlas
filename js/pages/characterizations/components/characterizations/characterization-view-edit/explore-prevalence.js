63bc661544d916d3d0ea6be052f4b97e831a4ec9
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2018-11-30 19:01:23 +0300

    Added explore prevalence with stratas (#1176)

--- js/pages/characterizations/components/characterizations/characterization-view-edit/explore-prevalence.js
+++ js/pages/characterizations/components/characterizations/characterization-view-edit/explore-prevalence.js
@@ -6,6 +6,7 @@ define([
 	'utils/AutoBind',
 	'pages/characterizations/services/CharacterizationService',
 	'./utils',
+	'numeral',
 	'less!./explore-prevalence.less',
 ], function(
 	ko,
@@ -15,6 +16,7 @@ define([
 	AutoBind,
 	CharacterizationService,
 	utils,
+	numeral,
 ){
 
 	class ExplorePrevalence extends AutoBind(Component) {
@@ -22,11 +24,9 @@ define([
 		constructor(params) {
 			super(params);
 			this.tableColumns = [
-				{ title: 'Relationship type', render: this.renderRelationship, width: '25%', },
-				{ title: 'Distance', data: 'distance', width: '10%', },
-				{ title: 'Concept name', data: 'covariateName', width: '45%', },
-				{ title: 'Count', data: 'count', width: '10%', },
-				{ title: 'Pct', data: 'avg', render: (val) => utils.formatPct(val * 100), width: '10%', },
+				{ title: 'Relationship type', render: this.renderRelationship, class: this.classes('col-type'), },
+				{ title: 'Distance', data: 'distance', class: this.classes('col-distance'), },
+				{ title: 'Concept name', data: 'covariateName', class: this.classes('col-concept'), },
 			];
 			this.data = ko.observableArray();
 			this.loading = ko.observable();
@@ -34,6 +34,7 @@ define([
 			this.cohortId = this.explore.cohortId;
 			this.cohortName = this.explore.cohortName;
 			this.exploring = ko.observable();
+			this.relations = ko.computed(() => this.prepareTabularData(this.data()));
 			this.exploringTitle = ko.pureComputed(() => this.exploring() ? 'Exploring concept hierarchy for: ' + this.exploring() : null );
 			this.loadData(this.explore);
 		}
@@ -45,8 +46,65 @@ define([
 				.finally(() => this.loading(false));
 		}
 
+		getCountColumn(strata) {
+			return {
+				title: 'Count',
+				class: this.classes('col-count'),
+				render: (s, p, d) => numeral(d.count[strata] || 0).format(),
+			};
+		}
+
+		getPctColumn(strata, idx) {
+			return {
+				title: 'Pct',
+				class: this.classes('col-pct'),
+				render: (s, p, d) => utils.formatPct(d.pct[strata] || 0),
+			};
+		}
+
+		prepareTabularData(data) {
+			const columns = [
+				...this.tableColumns,
+			];
+			let stratas = {
+			};
+			let stats = {};
+			data.forEach(st => {
+				if (stats[st.covariateId] === undefined) {
+					stats[st.covariateId] = {
+						...st,
+						count: {},
+						pct: {},
+					};
+				}
+				if (stratas[st.strataId] === undefined) {
+					stratas[st.strataId] = st.strataName || 'All stratas';
+				}
+				const stat = stats[st.covariateId];
+				stat.count[st.strataId] = st.count;
+				stat.pct[st.strataId] = st.avg;
+			});
+			Object.keys(stratas).forEach(strataId => {
+				columns.push(this.getCountColumn(strataId));
+				columns.push(this.getPctColumn(strataId));
+			});
+			stats = Object.values(stats);
+			stratas = Object.values(stratas);
+			return {
+				stats,
+				stratas,
+				columns,
+			};
+		}
+
 		exploreByFeature(data) {
-			this.loadData(data).then(() => this.exploring(data.covariateName));
+			this.loadData(data).then(() => {
+				if (data.covariateId !== this.explore.covariateId) {
+					this.exploring(data.covariateName);
+				} else {
+					this.exploring(null);
+				}
+			});
 		}
 
 		resetExploring() {
@@ -56,9 +114,9 @@ define([
 		renderRelationship(data, type, row) {
 			const distance = row.distance;
 			const rel = distance > 0 ? 'Ancestor' : distance < 0 ? 'Descendant' : 'Selected';
-			const cls = this.classes({element: 'explore', modifiers: distance === 0 ? 'disabled' : '', extra: 'btn btn-sm btn-primary'});
+			const cls = this.classes({element: 'explore', modifiers: distance === 0 ? 'disabled' : '' });
 			const binding = distance !== 0 ? 'click: () => $component.exploreByFeature({...$data, cohortId: $component.cohortId})' : '';
-			return "<span class='"+ cls + "' data-bind='" + binding + "'>Explore</span> " + rel;
+			return "<a class='"+ cls + "' data-bind='" + binding + "'>Explore</a> " + rel;
 		}
 
 	}
