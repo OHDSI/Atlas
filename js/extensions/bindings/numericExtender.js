8ddec4a236ddb823ace3ac93cba284fbcec5d481
Chris Knoll <cknoll@ohdsi.org>
2019-01-07 14:36:02 -0500

    Use d3.scaleThreshold() for color classification and covariate setting time window fixes. (#1207)
    * Use d3.scaleThreshold() for color classification.
    
    Fixes #732.
    
    * Covariate Settings time window fixes
    
    Change numeric fields to use value binding to allow negative values.
    Changed numeric extender to returnt he current value when invalid input is provided (previously resulted in value 0)
    
    Fixes #1169

--- js/extensions/bindings/numericExtender.js
+++ js/extensions/bindings/numericExtender.js
@@ -7,17 +7,14 @@ define(['knockout'], function (ko) {
                 const isNumeric = RegExp('^-?[0-9]*(?:\.[0-9]+)?$');
                 var current = target(),
                     roundingMultiplier = Math.pow(10, precision),
-                    newValueAsNum = isNumeric.test(newValue) ? +newValue : 0,
+                    newValueAsNum = isNumeric.test(newValue) ? +newValue : current,
                     valueToWrite = Math.round(newValueAsNum * roundingMultiplier) / roundingMultiplier;
     
                 //only write if it changed
                 if (valueToWrite !== current) {
-                    target(valueToWrite);
+									target(valueToWrite);
                 } else {
-                    //if the rounded value is the same, but a different value was written, force a notification for the current field
-                    if (newValue !== current) {
-                        target.notifySubscribers(valueToWrite);
-                    }
+									target.notifySubscribers(valueToWrite);
                 }
             }
         }).extend({ notify: 'always' });
