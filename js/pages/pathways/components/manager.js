467604832e88f9562130724f225301bbaf0db4cd
ASaltykov <alexander.saltykov@odysseusinc.com>
2019-01-22 14:40:41 +0300

    rollback

--- js/pages/pathways/components/manager.js
+++ js/pages/pathways/components/manager.js
@@ -55,7 +55,6 @@ define([
 				design: this.design,
 				analysisId: this.analysisId,
 				executionId: this.executionId,
-				model: params.model,
 			};
 			this.pathwayCaption = ko.computed(() => {
 				if (this.design() && this.design().id !== undefined && this.design().id !== 0) {
