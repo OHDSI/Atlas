17ed68da109247620884ea8a3055bc34819d712e
Anastasiia Klochkova <anastasiia.klochkova@odysseusinc.com>
2019-02-04 13:23:40 +0300

    renamed observable to be more readable; added conditions to hide blocks

--- js/pages/profiles/profile-manager.js
+++ js/pages/profiles/profile-manager.js
@@ -91,7 +91,7 @@ define([
 				this.cohortSource = ko.observable();
 				this.person = ko.observable();
 				this.loadingPerson = ko.observable(false);
-				this.cantFindPerson = ko.observable(false);
+				this.personIsFound = ko.observable(true);
 				this.shadedRegions = ko.observable([]);
 
 				this.setSourceKey = (d) => {
@@ -308,7 +308,7 @@ define([
 			}
 
 			loadPerson() {
-				this.cantFindPerson(false);
+				this.personIsFound(true);
 				this.loadingPerson(true);
 
 				let url = constants.paths.person(this.sourceKey(), this.personId());
@@ -358,7 +358,7 @@ define([
 						this.person(person);
 					})
 					.catch(() => {
-						this.cantFindPerson(true);
+						this.personIsFound(false);
 						this.loadingPerson(false);
 					});
 			}
