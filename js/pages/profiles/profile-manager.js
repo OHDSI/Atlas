b8c3debbd1271a6f8ac62978288d31afc7f1875e
Anastasiia Klochkova <anastasiia.klochkova@odysseusinc.com>
2019-02-05 20:34:37 +0300

    reverted conditions

--- js/pages/profiles/profile-manager.js
+++ js/pages/profiles/profile-manager.js
@@ -91,7 +91,7 @@ define([
 				this.cohortSource = ko.observable();
 				this.person = ko.observable();
 				this.loadingPerson = ko.observable(false);
-				this.cantFindPerson = ko.observable(true);
+				this.cantFindPerson = ko.observable(false);
 				this.shadedRegions = ko.observable([]);
 
 				this.setSourceKey = (d) => {
@@ -308,7 +308,7 @@ define([
 			}
 
 			loadPerson() {
-				this.cantFindPerson(true);
+				this.cantFindPerson(false);
 				this.loadingPerson(true);
 
 				let url = constants.paths.person(this.sourceKey(), this.personId());
@@ -358,7 +358,7 @@ define([
 						this.person(person);
 					})
 					.catch(() => {
-						this.cantFindPerson(false);
+						this.cantFindPerson(true);
 						this.loadingPerson(false);
 					});
 			}
