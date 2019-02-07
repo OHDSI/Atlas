23add1ba16c223f4cbe930b8c2ef5190aaa55b5a
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2019-02-07 13:29:29 +0300

    fixed Cancel button disabled state

--- js/pages/pathways/components/tabs/pathway-executions.js
+++ js/pages/pathways/components/tabs/pathway-executions.js
@@ -164,7 +164,7 @@ define([
 
 		generate(source) {
 			let confirmPromise;
-			this.stopping({...this.stopping(), [source.sourceKey]: false});
+			this.stopping({...this.stopping(), [source]: false});
 
 			const executionGroup = this.executionGroups().find(g => g.sourceKey === source);
 			if (!executionGroup) {
