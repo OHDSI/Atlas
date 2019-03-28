83fd45123cc47c55e39be703e8c775e2af1dfb04
AntonStepanof <48437569+AntonStepanof@users.noreply.github.com>
2019-03-28 21:58:40 +0300

    Fixes issues with first Source creation (#1614)

--- js/pages/cohort-definitions/cohort-definitions.js
+++ js/pages/cohort-definitions/cohort-definitions.js
@@ -44,7 +44,7 @@ define([
 		
 			this.isAuthenticated 	= authApi.isAuthenticated;
 			this.canReadCohorts 	= ko.pureComputed(() => (config.userAuthenticationEnabled && this.isAuthenticated() && authApi.isPermittedReadCohorts()) || !config.userAuthenticationEnabled);
-			this.canCreateCohort 	= ko.pureComputed(() => (config.userAuthenticationEnabled && this.isAuthenticated() && authApi.isPermittedCreateCohort()) || !config.userAuthenticationEnabled);			
+			this.canCreateCohort 	= ko.pureComputed(() => (config.userAuthenticationEnabled && this.isAuthenticated() && authApi.isPermittedCreateCohort()) || !config.userAuthenticationEnabled);
 		}
 
 		newDefinition(data, event) {
