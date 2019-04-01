8ef7226e8668230e0fa865f53ca80dd43bb3e082
AntonStepanof <An.Stepanof@yandex.ru>
2019-04-01 16:22:08 +0300

    update text view for cohort definition

--- js/pages/cohort-definitions/cohort-definition-manager.js
+++ js/pages/cohort-definitions/cohort-definition-manager.js
@@ -1220,7 +1220,41 @@ define(['jquery', 'knockout', 'text!./cohort-definition-manager.html',
 			}
 
 			copyTextViewToClipboard () {
-				this.copyToClipboard('#btnCopyTextViewClipboard', '#copyTextViewMessage');
+                let columns = [
+                    {
+                        title: 'Concept Id',
+                        data: 'CONCEPT_ID'
+                    },
+                    {
+                        title: 'Concept Name',
+                        data: 'CONCEPT_NAME'
+                    },
+                    {
+                        title: 'Domain',
+                        data: 'DOMAIN_ID'
+                    },
+                    {
+                        title: 'Vocabulary',
+                        data: 'VOCABULARY_ID'
+                    }
+                ];
+                let setsText = '';
+                this.sortedConceptSets().forEach((set) => {
+                    setsText += '\n' + set.name() + '\n';
+                    columns.forEach((c) => {
+                        setsText += c.title + ' ';
+                    });
+                    setsText += 'Excluded Descendants Mapped' + '\n';
+                    set.expression.items().forEach((item) => {
+                        columns.forEach((c) => {
+                            setsText += item.concept[c.data] + ' ';
+                        });
+                        setsText += (item.isExcluded() ? 'YES' : 'NO') + ' ';
+                        setsText += (item.includeDescendants() ? 'YES' : 'NO') + ' ';
+                        setsText += (item.includeMapped() ? 'YES' : 'NO') + ' \n';
+                    });
+                });
+                this.copyToClipboard('#btnCopyTextViewClipboard', '#copyTextViewMessage', setsText);
 			}
 
 			copyCohortExpressionJSONToClipboard () {
--- js/utils/Clipboard.js
+++ js/utils/Clipboard.js
@@ -23,7 +23,43 @@ define(['clipboard'], (clipboard) => {
 				console.log(e);
 			});
 		}
-	};
+
+
+        copyToClipboard(clipboardButtonId, clipboardButtonMessageId, appendedText) {
+            const currentClipboard = new clipboard(clipboardButtonId);
+            currentClipboard.on('success', (e) => {
+                console.log('Copied to clipboard');
+                e.clearSelection();
+                let selected = false;
+                let el = document.createElement('textarea');
+                el.value = e.text + appendedText;
+                el.setAttribute('readonly', '');
+                el.style.position = 'absolute';
+                el.style.left = '-9999px';
+                document.body.appendChild(el);
+                if (document.getSelection().rangeCount > 0) {
+                    selected = document.getSelection().getRangeAt(0)
+                }
+                el.select();
+                document.execCommand('copy');
+                document.body.removeChild(el);
+                if (selected) {
+                    document.getSelection().removeAllRanges();
+                    document.getSelection().addRange(selected);
+                }
+                $(clipboardButtonMessageId).fadeIn();
+                setTimeout(() => {
+                    $(clipboardButtonMessageId).fadeOut();
+                }, 1500);
+            });
+
+            currentClipboard.on('error', (e) => {
+                console.log('Error copying to clipboard');
+                console.log(e);
+            });
+        }
+
+    };
 
 	return Clipboard;
 });
