6c75b52c902cb101a204c5b1c2d348f931af4353
Anthony Sena <anthonysena@users.noreply.github.com>
2019-05-22 10:29:18 -0400

    Fixes display of cdmVersionRange on cohort expression - fixes #1734 (#1736)

--- js/components/cohortbuilder/CohortExpression.js
+++ js/components/cohortbuilder/CohortExpression.js
@@ -30,7 +30,7 @@ define(function (require, exports) {
 		self.CollapseSettings = {CollapseType: ko.observable(data.CollapseSettings && data.CollapseSettings.CollapseType || "ERA"), EraPad: ko.observable(data.CollapseSettings && data.CollapseSettings.EraPad || 0 ) }
 		self.CensorWindow = ko.observable(new Period(data.CensorWindow));
 
-		self.cdmVersion = data.cdmVersion || null;
+		self.cdmVersionRange = data.cdmVersionRange || null;
 	}
 	return CohortExpression;
 });
