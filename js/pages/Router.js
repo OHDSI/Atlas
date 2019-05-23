4dff7ea6103a2d8c9cd069f081f190be4b645391
AntonStepanof <An.Stepanof@yandex.ru>
2019-05-23 15:13:23 +0300

    added check for feedback screen for logout

--- js/pages/Router.js
+++ js/pages/Router.js
@@ -78,7 +78,7 @@ define(
         }, {});
         // anyway, we should track the moment when the user exits and check permissions once again
 				authApi.isAuthenticated.subscribe((isAuthenticated) => {
-					if (!isAuthenticated) {
+					if (!isAuthenticated && window.location.hash !== '#/feedback') {
             this.setCurrentView('white-page');
 						this.schedulePageUpdateOnLogin(this.activeRouteHandler);
 					}
