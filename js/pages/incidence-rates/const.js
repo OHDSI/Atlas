88d28012c655573b65b2fe0a7aeb4c28171345bc
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2019-05-24 19:14:53 +0300

    tootip added

--- js/pages/incidence-rates/const.js
+++ js/pages/incidence-rates/const.js
@@ -11,6 +11,11 @@ define(
         COMPLETE: 'COMPLETE',
     };
 
+    const disabledReasons = {
+      DIRTY: 'Save changes to generate',
+      ACCESS_DENIED: 'Access denied',
+    };
+
     function isInProgress(currentStatus) {
         return [status.PENDING, status.RUNNING].includes(currentStatus);
     }
@@ -19,6 +24,7 @@ define(
       apiPaths,
       status,
       isInProgress,
+      disabledReasons,
     };
   }
 );
