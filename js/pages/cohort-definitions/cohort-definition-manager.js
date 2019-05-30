9d0a4650540cb91a722adae20ad18c260a82d1e5
Pavel Grafkin <pavgra@gmail.com>
2019-05-29 20:24:46 -0400

    Fixes from master to 2.7.2 (#1797)

--- js/pages/cohort-definitions/cohort-definition-manager.js
+++ js/pages/cohort-definitions/cohort-definition-manager.js
@@ -975,6 +975,7 @@ define(['jquery', 'knockout', 'text!./cohort-definition-manager.html',
 					return;
 				}
 
+				var conceptSetItemsToAdd = sharedState.selectedConcepts();
 				items.forEach((item)=> {
 					var conceptSetItem = {};
 					conceptSetItem.concept = item.concept;
@@ -983,9 +984,10 @@ define(['jquery', 'knockout', 'text!./cohort-definition-manager.html',
 					conceptSetItem.includeMapped = ko.observable(item.includeMapped);
 
 					sharedState.selectedConceptsIndex[item.concept.CONCEPT_ID] = 1;
-					sharedState.selectedConcepts.push(conceptSetItem);
+					conceptSetItemsToAdd.push(conceptSetItem);
 				});
-				conceptSet.expression.items.valueHasMutated();
+				sharedState.selectedConcepts(conceptSetItemsToAdd);
+				this.model.loadCohortConceptSet(conceptSet.id, 'cohort-definition-manager', 'details');
 				this.clearImportConceptSetJson();
 			};
 
