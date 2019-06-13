ec0a77d48e11eaf9128a8b4e763870006cf38541
Pavel Grafkin <pavgra@gmail.com>
2019-06-13 05:31:09 -0400

    Added plugin registry & ability to inject new cohort reports

--- js/pages/cohort-definitions/components/reporting/cohort-reports/cohort-reports.js
+++ js/pages/cohort-definitions/components/reporting/cohort-reports/cohort-reports.js
@@ -1,6 +1,7 @@
 define([
 	'knockout',
 	'components/Component',
+	'services/PluginRegistry',
 	'./const',
 	'utils/CommonUtils',
 	'text!./cohort-reports.html',
@@ -9,41 +10,47 @@ define([
 ], function (
 	ko,
 	Component,
+	PluginRegistry,
 	constants,
 	commonUtils,
 	view
 ) {
+
+	PluginRegistry.add(constants.COHORT_REPORT_COMPONENT_TYPE, {
+		title: 'By Person',
+		priority: 1,
+		html: `
+			<feasibility-report-viewer-with-header params="{ sourceKey: sourceKey, cohortId: cohortId, reportType: ${constants.INCLUSION_REPORT.BY_PERSON} }">
+			</feasibility-report-viewer-with-header>
+		`
+	});
+
+	PluginRegistry.add(constants.COHORT_REPORT_COMPONENT_TYPE, {
+		title: 'By Events',
+		priority: 2,
+		html: `
+			<feasibility-report-viewer-with-header params="{ sourceKey: sourceKey, cohortId: cohortId, reportType: ${constants.INCLUSION_REPORT.BY_EVENT} }">
+			</feasibility-report-viewer-with-header>
+		`,
+	});
+
 	class CohortReports extends Component {
 		constructor(params) {
 			super();
 
-			this.params = params;
-
+			this.sourceKey = ko.computed(() => params.source() && params.source().sourceKey);
 			this.cohortId = ko.computed(() => params.cohort().id());
 
-			this.tabs = [
-				{
-					title: 'By Person',
-					componentName: 'feasibility-report-viewer-with-header',
-					componentParams: {
-						source: params.source,
-						cohortId: this.cohortId,
-						reportType: constants.INCLUSION_REPORT.BY_PERSON,
-					},
-				},
-				{
-					title: 'By Events',
-					componentName: 'feasibility-report-viewer-with-header',
-					componentParams: {
-						source: params.source,
-						cohortId: this.cohortId,
-						reportType: constants.INCLUSION_REPORT.BY_EVENT,
-					},
-				}
-			];
+			const componentParams =  {
+				sourceKey: this.sourceKey,
+				cohortId: this.cohortId
+			};
+
+			this.tabs = PluginRegistry.findByType(constants.COHORT_REPORT_COMPONENT_TYPE).map(t => ({ ...t, componentParams }));
 		}
 
 		dispose() {
+			this.sourceKey.dispose();
 			this.cohortId.dispose();
 		}
 	}
--- js/pages/cohort-definitions/components/reporting/cohort-reports/const.js
+++ js/pages/cohort-definitions/components/reporting/cohort-reports/const.js
@@ -1,10 +1,13 @@
 define([], () => {
+    const COHORT_REPORT_COMPONENT_TYPE = 'atlas-cohort-report';
+
     const INCLUSION_REPORT = {
     	BY_EVENT: 0,
     	BY_PERSON: 1,
     };
 
     return {
+    	COHORT_REPORT_COMPONENT_TYPE,
     	INCLUSION_REPORT
     };
   }
