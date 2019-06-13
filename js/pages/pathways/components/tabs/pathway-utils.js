e5818cde7f7a76fad3d692b8e938995e3964a0cc
Vlad Belousov <47257857+vlbe@users.noreply.github.com>
2019-06-13 15:10:21 +0300

    Disabled export for unsaved entities (#1884)

--- js/pages/pathways/components/tabs/pathway-utils.js
+++ js/pages/pathways/components/tabs/pathway-utils.js
@@ -1,13 +1,13 @@
 define([
-	'knockout', 
-	'../../PathwayService', 
-	'../../PermissionService', 
-	'text!./pathway-utils.html', 
-	'appConfig', 
-	'services/AuthAPI', 
-	'components/Component', 
-	'utils/AutoBind', 
-	'utils/CommonUtils', 
+	'knockout',
+	'../../PathwayService',
+	'../../PermissionService',
+	'text!./pathway-utils.html',
+	'appConfig',
+	'services/AuthAPI',
+	'components/Component',
+	'utils/AutoBind',
+	'utils/CommonUtils',
 	'less!./pathway-utils.less',
 ], function (
 	ko,
@@ -25,12 +25,13 @@ define([
 			super();
 
 			this.subscriptions = [];
-			
+
 			this.loading = ko.observable(false);
 
 			this.MODE_JSON = 0;
 			this.MODE_IMPORT = 1;
 
+			this.dirtyFlag = params.dirtyFlag;
 			this.analysisId = params.analysisId;
 			this.mode = ko.observable(this.MODE_JSON);
 
@@ -40,7 +41,7 @@ define([
 			this.exportEntity = ko.observable();
 			this.exportService = PathwayService.loadExportDesign;
 			this.importService = PathwayService.importPathwayDesign;
-			
+
 			// subscriptions
 			this.subscriptions.push(this.analysisId.subscribe((newVal) => {
 				this.loadExportJSON();
@@ -60,7 +61,7 @@ define([
 				this.loading(false);
 			}
 		}
-		
+
 		dispose() {
 			this.subscriptions.forEach(s => s.dispose());
 		}
