7533fa255781f5d5a5c24f12347d96dae8c51dd7
Vlad Belousov <vladislav.belousov@firstlinesoftware.com>
2019-06-18 11:39:31 +0300

    Added HemOnc license agreement

--- js/config/terms-and-conditions.js
+++ js/config/terms-and-conditions.js
@@ -1,7 +1,7 @@
 define(['text!./terms-and-conditions-content.html', 'less!./terms-and-conditions.less',], function (content) {
 	var termsAndConditions = {
     header: 'License Agreement',
-    description: 'In order to use the SNOMED International SNOMED CT Browser and HemOnc Vocabulary, please accept the following license agreement:',
+    description: 'In order to use the SNOMED International SNOMED CT Browser and HemOnc, please accept the following license agreement:',
     content,
     acceptanceExpiresInDays: 30,
   };
