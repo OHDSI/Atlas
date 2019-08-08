d7b3a21da0b18957060ee86630f6582aa3283efe
Pavel Grafkin <pavgra@gmail.com>
2019-08-08 05:47:00 -0700

    Fixes navigation inside IR (#2007)

--- js/pages/incidence-rates/routes.js
+++ js/pages/incidence-rates/routes.js
@@ -9,13 +9,8 @@ define(
             router.setCurrentView('ir-browser');
           });
         }),
-        '/iranalysis/new': new AuthorizedRoute(analysisId => {
-          require(['./ir-manager'], () => {
-            atlasState.IRAnalysis.selectedId(null);
-            router.setCurrentView('ir-manager', {});
-          });
-        }),
         '/iranalysis/:analysisId:/?((\w|.)*)': new AuthorizedRoute((analysisId, path) => {
+          analysisId = parseInt(analysisId);
           path = path.split("/");
           let activeTab = null;
           if (path.length > 0 && path[0] !== "") {
