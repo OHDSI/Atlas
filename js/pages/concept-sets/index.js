d4f567e61fb5158ceac9c84fc9a7cedfb3dd18cc
Vlad Belousov <vladislav.belousov@firstlinesoftware.com>
2019-08-20 12:33:27 +0300

    Fixed nav menu icons for cohort definitions and concept sets

--- js/pages/concept-sets/index.js
+++ js/pages/concept-sets/index.js
@@ -1,12 +1,31 @@
 define(
   (require, exports) => {
+    const ko = require('knockout');
+    const appState = require('atlas-state');
     const buildRoutes = require('./routes');
+    const constants = require('./const');
+
+    const statusCss = ko.pureComputed(() => {
+      if (appState.ConceptSet.current()) {
+        return appState.ConceptSet.dirtyFlag().isDirty() ? 'unsaved' : 'open';
+      }
+      return '';
+    });
+
+    const navUrl = ko.pureComputed(function () {
+      let url = "#/conceptsets";
+      if (appState.ConceptSet.current()) {
+        url = `#${constants.paths.mode(appState.ConceptSet.current().id || 0)}`;
+      }
+      return url;
+    });
+
     return {
       title: 'Concept Sets',
       buildRoutes,
-      navUrl: () => '#/conceptsets',
+      navUrl,
       icon: 'shopping-cart',
-			statusCss: () => ''
+			statusCss,
     };
   }
 );
