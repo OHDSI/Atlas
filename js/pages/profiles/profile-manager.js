4bb42dc9917e4cfbb50c6a278e5960bd217acc6b
Vlad Belousov <47257857+vlbe@users.noreply.github.com>
2019-09-19 21:13:37 +0300

    Fixed colors rendering in events highlight datatable (#2039)

--- js/pages/profiles/profile-manager.js
+++ js/pages/profiles/profile-manager.js
@@ -416,15 +416,17 @@ define([
 			}
 
 			setHighlights(colorIndex) {
-				var selectedData = $('#highlight-table table').DataTable().rows('.selected').data();
-				for (var i = 0; i < selectedData.length; i++) {
+				const dt = $('#highlight-table table').DataTable();
+				const rows = dt.rows('.selected');
+				const selectedData = rows.data();
+				for (let i = 0; i < selectedData.length; i++) {
 					selectedData[i].highlight(this.getHighlightBackground(colorIndex)); // set the swatch color
 					selectedData[i].recs.forEach(r => {
 						r.highlight = this.getHighlightBackground(colorIndex);
 						r.stroke = this.getHighlightColor(colorIndex);
 					}); // set the record colors
 				}
-
+				rows && rows[0] && rows[0].forEach(r => dt.row(r).invalidate());
 				this.highlightRecs.valueHasMutated();
 			};
 
@@ -437,7 +439,9 @@ define([
 			}
 
 			clearHighlights() {
-				const selectedData = $('#highlight-table table').DataTable().data();
+				const dt = $('#highlight-table table').DataTable();
+				const rows = dt.rows('.selected');
+				const selectedData = rows.data();
 				for (let i = 0; i < selectedData.length; i++) {
 					selectedData[i].highlight(this.defaultColor); // set the swatch color
 					selectedData[i].recs.forEach(r => {
@@ -445,7 +449,7 @@ define([
 						r.stroke = this.defaultColor; // set the record colors
 					})
 				}
-
+				rows && rows[0] && rows[0].forEach(r => dt.row(r).invalidate());
 				this.highlightRecs.valueHasMutated();
 			}
 
