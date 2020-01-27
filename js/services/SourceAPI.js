a3a21d2d3d59de076bbcf6c466409a6fbfa7b8f4
Long <khoathanglong@gmail.com>
2020-01-27 11:02:58 +0100

    fix Vocabulary URL is null #2

--- js/services/SourceAPI.js
+++ js/services/SourceAPI.js
@@ -68,7 +68,7 @@ define(function (require, exports) {
 
   async function initSourcesConfig() {
     try {
-		const [{data: sources}] = await Promise.all([
+		const [{data: sources}, {data: priorityDaimons}] = await Promise.all([
 			httpService.doGet(config.api.url + 'source/sources'),
 			httpService.doGet(config.api.url + 'source/daimon/priority'),
 		]);
@@ -76,7 +76,7 @@ define(function (require, exports) {
         if (sources.length === 0) {
             return constants.applicationStatuses.noSourcesAvailable;
         }
-        setSharedStateSources(sources, );
+        setSharedStateSources(sources, priorityDaimons);
         return constants.applicationStatuses.running;
 	} catch (e) {
         if (e.status !== 403) {
@@ -87,7 +87,7 @@ define(function (require, exports) {
 	}
   }
 
-  function setSharedStateSources(sources, priorityDaimons={}) {
+  function setSharedStateSources(sources, priorityDaimons) {
     sharedState.sources([]);
     var serviceCacheKey = getCacheKey();
     var sourceList = [];
