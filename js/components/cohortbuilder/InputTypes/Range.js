264fff511197b861a2d568de635dd6e0cdf1edd7
Sergey Suvorov <sergey.suvorov@odysseusinc.com>
2020-03-12 09:30:57 +0300

    timezone offset is substracted from date to avoid changing of date(#2148)

--- js/components/cohortbuilder/InputTypes/Range.js
+++ js/components/cohortbuilder/InputTypes/Range.js
@@ -10,10 +10,17 @@ define(['knockout'], function (ko) {
 		self.Op = ko.observable(data.Op || 'gt');
 	}
 
+	function getCorrectDate(value) {
+		// Prevent changing of string represantion due to timezone offset
+		// Without this code value "2020-03-29" in date picker will be converted to string "2020-03-28" or "2020-03-30"
+		const offset = value.getTimezoneOffset() * 60000;
+		return new Date(value.getTime() - offset);
+	}
+
 	Range.prototype.toJSON = function () {
 		return {
-			Value : this.Value instanceof Date ? (this.Value.toISOString().slice(0,10)) : this.Value,
-			Extent: this.Extent instanceof Date ? (this.Extent.toISOString().slice(0,10)) : this.Extent,
+			Value : this.Value instanceof Date ? (getCorrectDate(this.Value).toISOString().slice(0,10)) : this.Value,
+			Extent: this.Extent instanceof Date ? (getCorrectDate(this.Extent).toISOString().slice(0,10)) : this.Extent,
 			Op: this.Op
 		}
 	}
