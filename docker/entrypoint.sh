ffd34638f3c44f255f18dad706e01f0ee30fbdfe
Joris Borgdorff <github@blootsvoets.net>
2020-04-21 14:42:16 +0200

    Ensure that WEBAPI_URL ends with a slash

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,6 +2,17 @@
 
 set -e
 
+# make sure the WebAPI URL ends with a slash
+case $WEBAPI_URL in
+  # correct, no action
+  */)
+    ;;
+  # otherwise, add slash
+  *)
+    WEBAPI_URL="$WEBAPI_URL/"
+    ;;
+esac
+
 if [ -n "${WEBAPI_URL}" ]; then
   sed -i "s|http://localhost:8080/WebAPI/|$WEBAPI_URL|g" "$ATLAS_HOME/js/config-local.js"
 fi
