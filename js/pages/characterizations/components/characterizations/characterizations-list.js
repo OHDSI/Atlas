d571714a862729f8ca86f671128ac3e55b650060
SergeyNSukhanov <sergey.n.sukhanov@gmail.com>
2020-05-20 13:17:26 +0300

    fixes

--- js/pages/characterizations/components/characterizations/characterizations-list.js
+++ js/pages/characterizations/components/characterizations/characterizations-list.js
@@ -1,107 +1,104 @@
 define([
-    'knockout',
-    'pages/characterizations/services/CharacterizationService',
-    'pages/characterizations/services/PermissionService',
-    'text!./characterizations-list.html',
-    'appConfig',
-    'services/AuthAPI',
-    'components/Component',
-    'utils/CommonUtils',
-    'utils/DatatableUtils',
-    'pages/characterizations/const',
-    '../tabbed-grid',
-    'less!./characterizations-list.less',
-    'components/ac-access-denied',
+  "knockout",
+  "pages/characterizations/services/CharacterizationService",
+  "pages/characterizations/services/PermissionService",
+  "text!./characterizations-list.html",
+  "appConfig",
+  "services/AuthAPI",
+  "components/Component",
+  "utils/CommonUtils",
+  "utils/DatatableUtils",
+  "pages/characterizations/const",
+  "../tabbed-grid",
+  "less!./characterizations-list.less",
+  "components/ac-access-denied",
 ], function (
-    ko,
-    CharacterizationService,
-    PermissionService,
-    view,
-    config,
-    authApi,
-    Component,
-    commonUtils,
-    datatableUtils,
-    constants,
+  ko,
+  CharacterizationService,
+  PermissionService,
+  view,
+  config,
+  authApi,
+  Component,
+  commonUtils,
+  datatableUtils,
+  constants
 ) {
-    class Characterizations extends Component {
-        constructor(params) {
-            super();
+  class Characterizations extends Component {
+    constructor(params) {
+      super();
 
-            this.gridTab = constants.characterizationsTab;
+      this.gridTab = constants.characterizationsTab;
 
-            this.loading = ko.observable(false);
-            this.data = ko.observableArray();
+      this.loading = ko.observable(false);
+      this.data = ko.observableArray();
 
-            this.isGetCCListPermitted = PermissionService.isPermittedGetCCList;
-            this.isCreatePermitted = PermissionService.isPermittedCreateCC;
+      this.isGetCCListPermitted = PermissionService.isPermittedGetCCList;
+      this.isCreatePermitted = PermissionService.isPermittedCreateCC;
 
-            this.gridColumns = [
-                {
-                    title: ko.i18n('cc.list.columns.id', 'Id'),
-                    data: 'id'
-                },
-                {
-                    title: ko.i18n('cc.browser.table.columns.name', 'Name'),
-                    data: 'name',
-                    className: this.classes('tbl-col', 'name'),
-                    render: datatableUtils.getLinkFormatter(d => ({
-                        link: '#/cc/characterizations/' + d.id,
-                        label: d['name']
-                    })),
-                },
-                {
-                    title: ko.i18n('cc.browser.table.columns.created', 'Created'),
-                    className: this.classes('tbl-col', 'created'),
-                    render: datatableUtils.getDateFieldFormatter(),
-                },
-                {
-                    title: ko.i18n('cc.browser.table.columns.updated', 'Updated'),
-                    className: this.classes('tbl-col', 'updated'),
-                    render: datatableUtils.getDateFieldFormatter('updatedAt'),
-                },
-                {
-                    title: ko.i18n('cc.browser.table.columns.author', 'Author'),
-                    className: this.classes('tbl-col', 'author'),
-                    render: datatableUtils.getCreatedByFormatter(),
-                },
+      this.gridColumns = [
+        {
+          title: ko.i18n("columns.id", "Id"),
+          data: "id",
+        },
+        {
+          title: ko.i18n("columns.name", "Name"),
+          data: "name",
+          className: this.classes("tbl-col", "name"),
+          render: datatableUtils.getLinkFormatter((d) => ({
+            link: "#/cc/characterizations/" + d.id,
+            label: d["name"],
+          })),
+        },
+        {
+          title: ko.i18n("columns.created", "Created"),
+          className: this.classes("tbl-col", "created"),
+          render: datatableUtils.getDateFieldFormatter(),
+        },
+        {
+          title: ko.i18n("columns.updated", "Updated"),
+          className: this.classes("tbl-col", "updated"),
+          render: datatableUtils.getDateFieldFormatter("updatedAt"),
+        },
+        {
+          title: ko.i18n("columns.author", "Author"),
+          className: this.classes("tbl-col", "author"),
+          render: datatableUtils.getCreatedByFormatter(),
+        },
+      ];
+      this.gridOptions = {
+        Facets: [
+          {
+            caption: ko.i18n("facets.caption.created", "Created"),
+            binding: (o) => datatableUtils.getFacetForDate(o.createdAt),
+          },
+          {
+            caption: ko.i18n("facets.caption.updated", "Updated"),
+            binding: (o) => datatableUtils.getFacetForDate(o.updatedAt),
+          },
+          {
+            caption: ko.i18n("facets.caption.author", "Author"),
+            binding: datatableUtils.getFacetForCreatedBy,
+          },
+        ],
+      };
 
-            ];
-            this.gridOptions = {
-                Facets: [
-                    {
-                        'caption': ko.i18n('cc.list.facets.created', 'Created'),
-                        'binding': (o) => datatableUtils.getFacetForDate(o.createdAt)
-                    },
-                    {
-                        'caption': ko.i18n('cc.list.facets.updated', 'Updated'),
-                        'binding': (o) => datatableUtils.getFacetForDate(o.updatedAt)
-                    },
-                    {
-                        'caption': ko.i18n('cc.list.facets.author', 'Author'),
-                        'binding': datatableUtils.getFacetForCreatedBy,
-                    },
-                ]
-            };
-
-            this.isGetCCListPermitted() && this.loadData();
-        }
-
-        loadData() {
-            this.loading(true);
-            CharacterizationService
-                .loadCharacterizationList()
-                .then(res => {
-                    datatableUtils.coalesceField(res.content, 'updatedAt', 'createdAt');
-                    this.data(res.content);
-                    this.loading(false);
-                });
-        }
-
-        createCharacterization() {
-            commonUtils.routeTo('/cc/characterizations/0');
-        }
+      this.isGetCCListPermitted() && this.loadData();
     }
 
-    return commonUtils.build('characterizations-list', Characterizations, view);
+    loadData() {
+      this.loading(true);
+      CharacterizationService.loadCharacterizationList().then((res) => {
+        datatableUtils.coalesceField(res.content, "updatedAt", "createdAt");
+        this.data(res.content);
+        this.loading(false);
+      });
+    }
+
+    createCharacterization() {
+      commonUtils.routeTo("/cc/characterizations/0");
+    }
+  }
+
+  return commonUtils.build("characterizations-list", Characterizations, view);
 });
