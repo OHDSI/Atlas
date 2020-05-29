19b9291137ab11a5828420877ee1764a6bb28357
Sergey Suvorov <sergey.suvorov@odysseusinc.com>
2020-05-29 11:12:07 +0300

    Fix unique identifier for clipboard target(#2001)

--- js/utils/CommonUtils.js
+++ js/utils/CommonUtils.js
@@ -210,7 +210,7 @@ define([
 		return tooltipText.replace(/'/g, "\\'").replace(/"/g, '&quot;');
 	}
 
-	const getUniqueIdentifier = function() {
+	const getUniqueIdentifier = () => {
 		return ([1e7]+1e3+4e3+8e3+1e11).replace(/[018]/g,c=>(c^crypto.getRandomValues(new Uint8Array(1))[0]&15 >> c/4).toString(16));
 	}
 
