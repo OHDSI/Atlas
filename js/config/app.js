a4842f2ab2fd5465c4eb3fc0dd6e06a5a9aff2a8
Sergey Suvorov <sergey.suvorov@odysseusinc.com>
2020-08-11 11:09:48 +0300

    revert "userAuthenticationEnabled" to false (#2167)

--- js/config/app.js
+++ js/config/app.js
@@ -9,7 +9,7 @@ define(function () {
   appConfig.cacheSources = false;
   appConfig.pollInterval = 60000;
 	appConfig.cohortComparisonResultsEnabled = false;
-	appConfig.userAuthenticationEnabled = true;
+	appConfig.userAuthenticationEnabled = false;
 	appConfig.plpResultsEnabled = false;
 	appConfig.useExecutionEngine = false;
 	appConfig.viewProfileDates = false;
