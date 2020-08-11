08f44f7cbf6d0f069cba34b9d8a9245022c62b4b
Sergey Suvorov <sergey.suvorov@odysseusinc.com>
2020-08-11 13:32:19 +0300

    removed geo spatial code from atlas (#1445)

--- js/gis/cohort-report-geospatial/index.js
+++
@@ -1,9 +0,0 @@
-define(['./component.js'], (component) => {
-	document.dispatchEvent(new CustomEvent('registerAtlasPlugin', { detail: {
-		type: component.element.TYPE,
-		plugin: {
-			title: component.element.TITLE,
-			html: `<${component.name} data-bind="attr: { 'data-cohortid': $data.cohortId, 'data-sourcekey': $data.sourceKey }"></${component.name}>`
-		}
-	}}));
-});
--- js/gis/person-map/index.js
+++
@@ -1,9 +0,0 @@
-define(['./component.js'], (component) => {
-	document.dispatchEvent(new CustomEvent('registerAtlasPlugin', { detail: {
-		type: component.element.TYPE,
-		plugin: {
-			title: component.element.TITLE,
-			html: `<${component.name} data-bind="attr: { 'data-personid': $data.personId, 'data-sourcekey': $data.sourceKey, 'data-startdate': $data.startDate, 'data-enddate': $data.endDate }"></${component.name}>`
-		}
-	}}));
-});
