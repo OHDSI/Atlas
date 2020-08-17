6a2b3bd14bc8c3689236c47bd1d7b95e2796829f
anton.abushkevich <anton.abushkevich>
2020-08-17 19:28:19 +0300

    (#2284) PLP/PLE export error

--- js/pages/prediction/prediction-manager.js
+++ js/pages/prediction/prediction-manager.js
@@ -400,7 +400,13 @@ define([
 		loadAnalysisFromServer(analysis) {
 			var header = analysis.json;
 			var specification = JSON.parse(analysis.data.specification);
-			this.patientLevelPredictionAnalysis(new PatientLevelPredictionAnalysis({ ...specification, ...header }));
+			const { createdBy, modifiedBy, ...props } = header;
+			this.patientLevelPredictionAnalysis(new PatientLevelPredictionAnalysis({
+				...specification,
+				...props,
+				createdBy: createdBy ? createdBy.name : null,
+				modifiedBy: modifiedBy ? modifiedBy.name : null
+			}));
 			this.packageName(header.packageName);
 			this.setUserInterfaceDependencies();
 			this.setAnalysisSettingsLists();
@@ -456,9 +462,9 @@ define([
 			const createdDate = commonUtils.formatDateForAuthorship(this.patientLevelPredictionAnalysis().createdDate);
 			const modifiedDate = commonUtils.formatDateForAuthorship(this.patientLevelPredictionAnalysis().modifiedDate);
 			return {
-					createdBy: lodash.get(this.patientLevelPredictionAnalysis(), 'createdBy.name'),
+					createdBy: lodash.get(this.patientLevelPredictionAnalysis(), 'createdBy'),
 					createdDate,
-					modifiedBy: lodash.get(this.patientLevelPredictionAnalysis(), 'modifiedBy.name'),
+					modifiedBy: lodash.get(this.patientLevelPredictionAnalysis(), 'modifiedBy'),
 					modifiedDate,
 			}
 		}
