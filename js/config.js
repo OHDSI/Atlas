535cb795105641cb0f032120749f4155ff53d2b8
Sergey Suvorov <sergey.suvorov@firstlinesoftware.com>
2020-08-19 20:52:05 +0300

    merge geo spatial into 2.8 (#2183)
    * merge geo spatial into 2.8 (https://github.com/OHDSI/WebAPI/issues/1445)
    
    * revert user auth to false (https://github.com/OHDSI/WebAPI/issues/1445)
    
    * add geospacial documentation to the Readme
    
    * fixes typo
    
    * moved Geospatial readme to a separate file
    
    * removed geo spatial code from atlas (#1445)
    
    * removed geo spatial code from atlas (#1445)
    
    * removed geo spatial code from atlas (#1445)
    
    * removed geo spatial code from atlas (#1445)
    
    Co-authored-by: Sergey Suvorov <sergey.suvorov@odysseusinc.com>
    Co-authored-by: wivern <vitaly.koulakov@gmail.com>

--- js/config.js
+++ js/config.js
@@ -1,21 +1,29 @@
 define([
 	'./config/app',
 	'./config/terms-and-conditions',
-	'optional!config-local'
-], function (app, termsAndConditions, localConfig) {
+	'optional!config-local',
+	'optional!config-gis',
+	'lodash',
+], function (app, termsAndConditions, localConfig, gisConfig, lodash) {
 
 	if (JSON.stringify(localConfig) == JSON.stringify({})) {
 		console.warn('Local configuration not found.  Using default values. To use a local configuration and suppress 404 errors, create a file called config-local.js under the /js directory');
 	}
 
-	let config = Object.assign({},
-		app,
+	const configs = [app,
 		termsAndConditions,
-		localConfig
-	);
+		localConfig,
+		gisConfig, 
+	]
+	const config = configs.reduce((accumulator, currentValue) => _.mergeWith(accumulator, currentValue, customizer));
+
+	function customizer(objValue, srcValue, key) {
+		if (key === 'externalLibraries' && _.isArray(objValue)) {
+			return objValue.concat(srcValue);
+		}
+	};
 
 	config.webAPIRoot = config.api.url;
 
 	return config;
-
 });
