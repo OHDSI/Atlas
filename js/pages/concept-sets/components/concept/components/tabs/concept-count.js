261aacbb0aa5139ac0e2fd191b2f38d7332cfd5e
Sergey Suvorov <sergey.suvorov@firstlinesoftware.com>
2020-10-29 13:40:34 +0300

    concept ancestors and descendants are loaded when tab is opened (#2360)
    * concept ancestors and descendants are loaded when tab is opened(#2042)
    
    * concept ancestors and descendants are loaded when tab is opened(#2042)
    
    * tabs are used now(#2042)
    
    * restructured concept manager
    
    * refactored try-finally
    
    Co-authored-by: Sergey Suvorov <sergey.suvorov@odysseusinc.com>
    Co-authored-by: wivern <vitaly.koulakov@gmail.com>

---
+++ js/pages/concept-sets/components/concept/components/tabs/concept-count.js
@@ -0,0 +1,81 @@
+define([
+	'knockout',
+	'text!./concept-count.html',
+	'components/Component',
+	'utils/CommonUtils',
+	'atlas-state',
+	'services/http',
+	'services/AuthAPI',
+	'pages/concept-sets/PermissionService',
+], function (
+	ko,
+	view,
+	Component,
+	commonUtils,
+	sharedState,
+	httpService,
+	authApi,
+	PermissionService,
+) {
+	class ConceptManager extends Component {
+		constructor(params) {
+			super(params);
+			this.currentConceptId = params.currentConceptId;
+			this.hasInfoAccess = params.hasInfoAccess;
+			this.isAuthenticated = params.isAuthenticated;
+			this.hasRCAccess = ko.computed(() => this.hasInfoAccess() && PermissionService.isPermittedGetRC(sharedState.sourceKeyOfVocabUrl()));
+
+			// this.commonUtils = commonUtils;
+			this.sourceCounts = ko.observableArray();
+			this.isLoading = ko.observable(false);
+
+			this.loadRecordCounts();
+		}
+
+		async fetchRecordCounts(sources) {
+			const promises = [];
+			const sourceData = [];
+			for (const source of sources) {
+				const { sourceName, sourceKey, resultsUrl } = source;
+				if (authApi.hasSourceAccess(sourceKey)) {
+					// await is harmless here since it will pull data sequentially while it can be done in parallel
+					let promise = httpService.doPost(`${resultsUrl}conceptRecordCount`, [this.currentConceptId()]).then(({ data }) => {
+						const recordCountObject = data.length > 0 ? Object.values(data[0])[0] : null;
+						if (recordCountObject) {
+							sourceData.push({
+								sourceName,
+								recordCount: recordCountObject[0],
+								descendantRecordCount: recordCountObject[1]
+							});
+						}
+					}).catch(err => {
+						const failedMsg = 'Failed to load data';
+						sourceData.push({
+							sourceName,
+							recordCount: failedMsg,
+							descendantRecordCount: failedMsg,
+						});
+					});
+					promises.push(promise);
+				}
+			}
+			
+			// Promise.allSettled works since Chrome v76 so we need polyfill for it
+			await Promise.allSettled(promises);
+			return sourceData;
+		}
+
+		async loadRecordCounts() {
+			this.isLoading(true);
+			try {
+					const sourcesWithResults = sharedState.sources().filter(source => source.hasResults);
+					const sourceData = await this.fetchRecordCounts(sourcesWithResults);
+					this.sourceCounts(sourceData);
+			} finally {
+					this.isLoading(false);
+			}
+		}
+	}
+
+	return commonUtils.build('concept-count', ConceptManager, view);
+});
---
+++ js/pages/concept-sets/components/concept/concept-manager.js
@@ -0,0 +1,115 @@
+define([
+	'knockout',
+	'text!./concept-manager.html',
+	'pages/Page',
+	'utils/AutoBind',
+	'services/Vocabulary',
+	'utils/CommonUtils',
+	'services/ConceptSet',
+	'components/conceptset/ConceptSetStore',
+  'components/conceptset/utils',
+	'utils/Renderers',
+	'atlas-state',
+	'services/http',
+	'../../const',
+	'services/AuthAPI',
+	'../../PermissionService',
+	'const',
+	'faceted-datatable',
+	'components/heading',
+	'components/conceptLegend/concept-legend',
+	'components/conceptAddBox/concept-add-box',
+	'less!./concept-manager.less',
+	'components/tabs',
+	'./components/tabs/concept-details',
+	'./components/tabs/concept-related',
+	'./components/tabs/concept-hierarchy',
+	'./components/tabs/concept-count',
+], function (
+	ko,
+	view,
+	Page,
+	AutoBind,
+	vocabularyProvider,
+	commonUtils,
+	conceptSetService,
+	ConceptSetStore,
+  conceptSetUtils,
+	renderers,
+	sharedState,
+	httpService,
+	constants,
+	authApi,
+	PermissionService,
+	globalConstants,
+) {
+	class ConceptManager extends AutoBind(Page) {
+		constructor(params) {
+			super(params);
+			this.currentConceptId = ko.observable();
+			this.currentConcept = ko.observable();
+
+			this.isLoading = ko.observable(false);
+			this.isAuthenticated = authApi.isAuthenticated;
+			this.hasInfoAccess = ko.computed(() => PermissionService.isPermittedGetInfo(sharedState.sourceKeyOfVocabUrl(), this.currentConceptId()));
+
+			this.tabParams = ko.observable({
+				currentConcept: this.currentConcept,
+				currentConceptId: this.currentConceptId,
+				hasInfoAccess: this.hasInfoAccess,
+				isAuthenticated: this.isAuthenticated,
+				addConcepts: this.addConcepts.bind(this),
+			});
+		}
+
+		async onPageCreated() {
+			this.currentConceptId(this.routerParams.conceptId);
+			this.loadConcept(this.currentConceptId());
+			super.onPageCreated();
+		}
+
+		onRouterParamsChanged({ conceptId }) {
+			if (conceptId !== this.currentConceptId() && conceptId !== undefined) {
+				this.currentConceptId(conceptId);
+				this.loadConcept(this.currentConceptId());
+			}
+		}
+
+		addConcept(options, conceptSetStore = ConceptSetStore.repository()) {
+			// add the current concept
+			const items = commonUtils.buildConceptSetItems([this.currentConcept()], options);
+			conceptSetUtils.addItemsToConceptSet({items, conceptSetStore});
+		}
+		
+		// produces a closure to wrap options and source around a function
+		// that accepts the source selected concepts list
+		addConcepts(options, conceptSetStore = ConceptSetStore.repository()) {
+			return (conceptsArr, isCurrentConcept = false) => {
+				const concepts = commonUtils.getSelectedConcepts(conceptsArr);
+				const items = commonUtils.buildConceptSetItems(concepts, options);
+				conceptSetUtils.addItemsToConceptSet({items, conceptSetStore});
+				commonUtils.clearConceptsSelectionState(conceptsArr);
+			}
+		}
+
+		enhanceConcept(concept) {
+			return {
+				...concept,
+				isSelected: ko.observable(false),
+			};
+		}
+
+		async loadConcept(conceptId) {
+			this.isLoading(true);
+			if (!this.hasInfoAccess()) {
+				return;
+			}
+
+			const { data } = await httpService.doGet(sharedState.vocabularyUrl() + 'concept/' + conceptId);
+			this.currentConcept(this.enhanceConcept(data));
+			this.isLoading(false);
+		}
+	}
+
+	return commonUtils.build('concept-manager', ConceptManager, view);
+});
