90846c79c50c182b3aab2582466784c38e981b96
Vlad Belousov <vladislav.belousov@odysseusinc.com>
2021-01-22 14:49:25 +0300

    Fixed default values for TimeAtRisk

--- js/pages/incidence-rates/inputTypes/TimeAtRisk.js
+++ js/pages/incidence-rates/inputTypes/TimeAtRisk.js
@@ -3,7 +3,7 @@ define(['knockout', './FieldOffset'], function (ko, FieldOffset) {
 	function TimeAtRisk(data) {
 		var self = this;
 		data = data || {};
-		console.log(data, 'd');
+
 		self.start = new FieldOffset(data.start, 'StartDate');
 		self.end = new FieldOffset(data.end, 'EndDate');
 
