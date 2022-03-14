e3e71d76bf1203654a917d571d282bc96d8f5a4d
anton-abushkevich <anton.abushkevich@gmail.com>
2022-03-14 15:26:51 +0300

    ATLP-800 Several equal parameters in Reusables editor (#2677)

--- js/pages/reusables/components/tabs/reusable-design.js
+++ js/pages/reusables/components/tabs/reusable-design.js
@@ -106,7 +106,7 @@ define([
 
         createNewParameter() {
             let newParamId = 1;
-            this.design().parameters().map(p => p.id).sort().some(id => {
+            this.design().parameters().map(p => p.id).sort((a, b) => a - b).some(id => {
                 if (id !== newParamId) {
                     return true;
                 } else {
