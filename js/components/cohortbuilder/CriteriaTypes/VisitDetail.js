d4db9c0c88c170724450fc87590244ef91904f66
MakTakin <semyon.titarenko@odysseusinc.com>
2022-07-28 15:07:49 +0300

    prepare visit details

---
+++ js/components/cohortbuilder/CriteriaTypes/VisitDetail.js
@@ -0,0 +1,69 @@
+define(['knockout', './Criteria', '../InputTypes/Range','conceptpicker/InputTypes/Concept', '../InputTypes/Text'], function (ko, Criteria, Range, Concept, Text) {
+
+	function VisitDetail(data, conceptSets) {
+		var self = this;
+		data = data || {};
+
+		Criteria.call(this, data, conceptSets);
+		console.log(data)
+		// set up subscription to update CodesetId and VisitSourceConcept if the item is removed from conceptSets
+		conceptSets.subscribe(function (changes) {
+			changes.forEach(function(change) {
+					if (change.status === 'deleted') {
+						if (ko.utils.unwrapObservable(self.CodesetId) == change.value.id)
+							self.CodesetId(null);
+						if (ko.utils.unwrapObservable(self.VisitSourceConcept()) == change.value.id)
+							self.VisitSourceConcept()(null);
+					}
+			});
+		}, null, "arrayChange");
+
+		// General Condition Occurence Criteria
+
+		// Verbatim fields
+		self.CodesetId = ko.observable(data.CodesetId);
+
+		self.OccurrenceStartDate = ko.observable(data.OccurrenceStartDate && new Range(data.OccurrenceStartDate));
+		self.OccurrenceEndDate = ko.observable(data.OccurrenceEndDate && new Range(data.OccurrenceEndDate));
+		self.VisitType = ko.observable(data.VisitType && ko.observableArray(data.VisitType.map(function (d) {
+			return new Concept(d);
+		})));
+		self.VisitTypeExclude = ko.observable(data.VisitTypeExclude || null);				
+		self.VisitSourceConcept = ko.observable(data.VisitSourceConcept != null ? ko.observable(data.VisitSourceConcept) : null);
+		self.VisitLength = ko.observable(data.VisitLength && new Range(data.VisitLength));
+
+		// Derived Fields
+		self.First = ko.observable(data.First || null);
+		self.Age = ko.observable(data.Age && new Range(data.Age));
+
+		// Linked Fields
+		self.Gender = ko.observable(data.Gender && ko.observableArray(data.Gender.map(function (d) {
+			return new Concept(d);
+		})));
+
+	  /* Do we still need prior enroll days inside the individual criteria?
+		self.PriorEnrollDays = ko.observable((typeof data.PriorEnrollDays == "number") ? data.PriorEnrollDays : null);
+		self.AfterEnrollDays = ko.observable((typeof data.AfterEnrollDays == "number") ? data.AfterEnrollDays : null);
+		*/
+	 
+		self.ProviderSpecialty = ko.observable(data.ProviderSpecialty && ko.observableArray(data.ProviderSpecialty.map(function (d) {
+			return new Concept(d);
+		})));
+
+		self.PlaceOfService = ko.observable(data.PlaceOfService && ko.observableArray(data.PlaceOfService.map(function (d) {
+			return new Concept(d);
+		})));
+
+		self.PlaceOfServiceLocation = ko.observable(data.PlaceOfServiceLocation != null ? ko.observable(data.PlaceOfServiceLocation) : null);
+		self.PlaceOfServiceDistance = ko.observable(data.PlaceOfServiceDistance && new Range(data.PlaceOfServiceDistance));
+	}
+
+	VisitDetail.prototype = new Criteria();
+	VisitDetail.prototype.constructor = VisitDetail;
+	VisitDetail.prototype.toJSON = function () {
+		return this;
+	}
+	
+	return VisitDetail;
+
+});
