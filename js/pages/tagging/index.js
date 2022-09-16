e00d1a4a6c3c4db0d813b32960cdaaa8b6769510
Anton Abushkevich <anton.abushkevich@gmail.com>
2022-09-16 11:48:23 +0300

    Tagging multi-assignment - hide Tagging section by default

--- js/pages/tagging/index.js
+++ js/pages/tagging/index.js
@@ -1,14 +1,16 @@
 define(
-  (require, exports) => {
+  (require) => {
     const ko = require('knockout');
     const buildRoutes = require('./routes');
+    const appConfig = require('config/app');
 
     return {
       title: ko.i18n('navigation.tagging', 'Tagging'),
       buildRoutes,
       navUrl: () => '#/tagging',
       icon: 'tags',
-			statusCss: () => ''
+	  statusCss: () => '',
+      hidden: !appConfig.enableTaggingSection
     };
   }
 );
