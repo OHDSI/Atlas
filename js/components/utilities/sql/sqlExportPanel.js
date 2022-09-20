11969f87ee41e9e437844dfa8d9f2e57b3974943
MakTakin <semyon.titarenko@odysseusinc.com>
2022-09-20 16:59:18 +0300

    added sql-rendered output

--- js/components/utilities/sql/sqlExportPanel.js
+++ js/components/utilities/sql/sqlExportPanel.js
@@ -6,7 +6,11 @@ define([
 	'utils/CommonUtils',
 	'utils/HighLightUtils',
 	'services/CohortDefinition',
+	'atlas-state',
+	'services/AuthAPI',
+	'./sqlExportPanelConfig',
 	'prism',
+	'less!./sqlExportPanel.less',
 ], function (
 	ko,
 	view,
@@ -15,6 +19,9 @@ define([
 	commonUtils,
 	highlightJS,
 	cohortService,
+	sharedState,
+	authApi,
+	defaultInputParamsValues
 ) {
 
 	class SqlExportPanel extends AutoBind(Component) {
@@ -26,12 +33,31 @@ define([
 			const { sql, templateSql, dialect, clipboardTarget } = params;
 			this.dialect = dialect;
 			this.loading = ko.observable();
-			this.sqlText = highlightJS(sql,'sql') || ko.observable();
+			this.sqlText = ko.observable(highlightJS(sql,'sql'));
 			this.templateSql = templateSql || ko.observable();
 			this.templateSql() && this.translateSql();
+			this.sqlParams = ko.computed(() => this.sqlParamsList(this.sqlText() || this.templateSql()));
 			this.clipboardTarget = clipboardTarget;
 			this.subscriptions = [];
 			this.subscriptions.push(this.templateSql.subscribe(v => !!v && this.translateSql()));
+			this.sourceSql = ko.observable(true);
+			this.paramsTemplateSql = ko.observable(this.sqlText);
+			this.currentResultSource = ko.observable();
+			this.resultSources = ko.computed(() => {
+				const resultSources = [];
+				sharedState.sources().forEach((source) => {
+					if (source.hasResults && authApi.isPermittedAccessSource(source.sourceKey)) {
+						resultSources.push(source);
+						if (source.resultsUrl === sharedState.resultsUrl()) {
+							this.currentResultSource(source);
+						}
+					}
+				})
+
+				return resultSources;
+			});
+			this.inputParamsValues = ko.observable(this.defaultParamsValue(this.currentResultSource));
+			this.subscriptions.push(this.sqlParams.subscribe(v => !!v && this.inputParamsValues(this.defaultParamsValue(this.currentResultSource))));
 		}
 
 		dispose() {
@@ -48,6 +74,48 @@ define([
 					this.loading(false);
 				}
 			}
+		}
+
+		sqlParamsList(templateSql) {
+			const regexp = /@[-\w]+/g;
+			const params = templateSql.match(regexp);
+			const paramsList = new Set(params);
+			return [...paramsList];
+		}
+
+		onSwitch() {
+			this.sourceSql(!this.sourceSql());
+		}
+
+		onChangeParamsValue() {
+			let templateText = this.sqlText();
+			for (const param in this.inputParamsValues()) {
+				if (!!this.inputParamsValues()[param].length) {
+					templateText = templateText.replaceAll(param, this.inputParamsValues()[param])
+				}
+			}
+			this.paramsTemplateSql(templateText);
+		}
+
+		defaultParamsValue(source) {
+			const daimons = source().daimons;
+			const inputParams = {};
+			this.sqlParams().forEach(param => {
+				const defaultParam = daimons.find(daimon => daimon.daimonType === defaultInputParamsValues[param]);
+
+				if (!!defaultParam) {
+					inputParams[param] = defaultParam.tableQualifier;
+				} else {
+					inputParams[param] = "";
+				}
+			});
+			return inputParams;
+		}
+
+		changeSource() {
+			const inputParams = this.defaultParamsValue(this.currentResultSource);
+			this.inputParamsValues(inputParams);
+			this.onChangeParamsValue();
 		}
 	}
 
