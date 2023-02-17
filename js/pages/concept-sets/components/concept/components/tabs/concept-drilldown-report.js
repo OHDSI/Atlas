12d5becf293a9f1adac61423133e042109cf4c19
ssuvorov-fls <sergey.suvorov@odysseusinc.com>
2023-02-17 10:55:45 +0300

    loading record counts was removed from concept drilldown tab

--- js/pages/concept-sets/components/concept/components/tabs/concept-drilldown-report.js
+++ js/pages/concept-sets/components/concept/components/tabs/concept-drilldown-report.js
@@ -51,62 +51,6 @@ define([
 
 				return resultSources;
 			});
-			this.subscriptions.push(this.currentSource.subscribe(r => {
-				if (r) {
-					this.loadRecordCounts()
-				}
-			}));
-
-			this.loadRecordCounts();
-		}
-
-		async fetchRecordCounts(sources) {
-			const promises = [];
-			const sourceData = [];
-			for (const source of sources) {
-				const { sourceName, sourceKey, resultsUrl } = source;
-				if (authApi.hasSourceAccess(sourceKey)) {
-					// await is harmless here since it will pull data sequentially while it can be done in parallel
-					let promise = httpService.doPost(`${resultsUrl}conceptRecordCount`, [this.currentConceptId()]).then(({ data }) => {
-						const recordCountObject = data.length > 0 ? Object.values(data[0])[0] : null;
-						if (recordCountObject) {
-
-							sourceData.push({
-								sourceName,
-								recordCount: recordCountObject[0],
-								descendantRecordCount: recordCountObject[1]
-							});
-						}
-
-					}).catch(err => {
-						const failedMsg = ko.i18n('cs.manager.concept.tabs.recordCounts.failedToLoadData', 'Failed to load data');
-						sourceData.push({
-							sourceName,
-							recordCount: failedMsg,
-							descendantRecordCount: failedMsg,
-						});
-					});
-					promises.push(promise);
-				}
-			}
-			
-			// Promise.allSettled works since Chrome v76 so we need polyfill for it
-			await Promise.allSettled(promises);
-			return sourceData;
-		}
-
-		async loadRecordCounts() {
-			this.isLoading(true);
-			try {
-
-				const sourcesWithResults = sharedState.sources().filter(source => source.hasResults);
-
-				const sourceData = await this.fetchRecordCounts(sourcesWithResults);
-
-				this.sourceCounts(sourceData);
-			} finally {
-				this.isLoading(false);
-			}
 		}
 
 		setCurrentReport(concept, reports) {
