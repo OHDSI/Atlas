ba299f78cbc32eda7e11796d5a8ebc3c8a923935
Juan Carlos Namendi Pineda <juan.namendi@odysseusinc.com>
2024-01-15 13:58:31 -0600

    adjust filter cohort definitions with add more options of timer filter

--- js/components/cohortbuilder/InputTypes/Window.js
+++ js/components/cohortbuilder/InputTypes/Window.js
@@ -6,16 +6,42 @@ define(['knockout'], function (ko) {
 
 		self.Start = {
 			Days: ko.observable((data.Start && data.Start.Days) === 0 ? 0 : (data.Start && data.Start.Days) || null),
-			Coeff: ko.observable((data.Start && data.Start.Coeff) === 0 ? 0 : (data.Start && data.Start.Coeff) || -1)
+			Coeff: ko.observable((data.Start && data.Start.Coeff) === 0 ? 0 : (data.Start && data.Start.Coeff) || -1),
+			TimeUnit: ko.observable((data.Start && data.Start.TimeUnit) || 'day'),
+			TimeUnitValue: ko.observable((data.Start && data.Start.TimeUnitValue) || null),
 		};
 		
 		self.End = {
 			Days: ko.observable((data.End && data.End.Days) === 0 ? 0 : (data.End && data.End.Days) || null),
-			Coeff: ko.observable((data.End && data.End.Coeff) === 0 ? 0 : (data.End && data.End.Coeff) || 1)
+			Coeff: ko.observable((data.End && data.End.Coeff) === 0 ? 0 : (data.End && data.End.Coeff) || 1),
+			TimeUnit: ko.observable((data.End && data.End.TimeUnit) ||  'day'),
+			TimeUnitValue: ko.observable((data.End && data.End.TimeUnitValue) || null),
 		};
 		
 		self.UseIndexEnd = ko.observable(data.UseIndexEnd || false);
 		self.UseEventEnd = ko.observable(data.UseEventEnd || false);
+
+		self.Start.TimeUnitValue.subscribe(function (newValue){
+			const insertValue = newValue ? newValue.toString().replace(/[\D\.]/g, '') : null;
+			self.Start.TimeUnitValue(insertValue ? Number(insertValue) : null);
+			self.Start.TimeUnit() === 'day' &&	self.Start.Days(insertValue ? Number(insertValue) : null);
+		})
+
+		self.End.TimeUnitValue.subscribe(function (newValue){
+			const insertValue = newValue ? newValue.toString().replace(/[\D\.]/g, '') : null;
+			self.End.TimeUnitValue(insertValue ? Number(insertValue) : null);
+			self.End.TimeUnit() === 'day' && self.End.Days(insertValue ? Number(insertValue) : null);
+		})
+
+		self.Start.TimeUnit.subscribe(function (newValue){
+			self.End.TimeUnit(newValue);
+			self.Start.Days(newValue === 'day' ? self.Start.TimeUnitValue() : null);
+		})
+
+		self.End.TimeUnit.subscribe(function (newValue){
+			self.Start.TimeUnit(newValue);
+			self.End.Days(newValue === 'day' ? self.End.TimeUnitValue() : null);
+		})
 		
 	}
 
