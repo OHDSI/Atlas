02367e195d8939d6d8121ed19455cd175e4d4834
Juan Carlos Namendi Pineda <juan.namendi@odysseusinc.com>
2024-02-01 10:54:58 +0700

    Fix can not adjust filter value to 0 in inclusion criteria

--- js/components/cohortbuilder/InputTypes/Window.js
+++ js/components/cohortbuilder/InputTypes/Window.js
@@ -8,29 +8,29 @@ define(['knockout'], function (ko) {
 			Days: ko.observable((data.Start && data.Start.Days) === 0 ? 0 : (data.Start && data.Start.Days) || null),
 			Coeff: ko.observable((data.Start && data.Start.Coeff) === 0 ? 0 : (data.Start && data.Start.Coeff) || -1),
 			TimeUnit: ko.observable((data.Start && data.Start.TimeUnit) || 'day'),
-			TimeUnitValue: ko.observable((data.Start && data.Start.TimeUnitValue) || null)
+			TimeUnitValue: ko.observable((data.Start && data.Start.TimeUnitValue) === 0 ? 0 : (data.Start && data.Start.TimeUnitValue)|| null)
 		};
 		
 		self.End = {
 			Days: ko.observable((data.End && data.End.Days) === 0 ? 0 : (data.End && data.End.Days) || null),
 			Coeff: ko.observable((data.End && data.End.Coeff) === 0 ? 0 : (data.End && data.End.Coeff) || 1),
 			TimeUnit: ko.observable((data.End && data.End.TimeUnit) ||  'day'),
-			TimeUnitValue: ko.observable((data.End && data.End.TimeUnitValue) || null)
+			TimeUnitValue: ko.observable((data.End && data.End.TimeUnitValue) === 0 ? 0 : (data.End && data.End.TimeUnitValue) || null)
 		};
 		
 		self.UseIndexEnd = ko.observable(data.UseIndexEnd || false);
 		self.UseEventEnd = ko.observable(data.UseEventEnd || false);
 
 		self.Start.TimeUnitValue.subscribe(function (newValue){
-			const insertValue = newValue ? newValue.toString().replace(/[\D\.]/g, '') : null;
-			self.Start.TimeUnitValue(insertValue ? Number(insertValue) : null);
-			self.Start.TimeUnit() === 'day' &&	self.Start.Days(insertValue ? Number(insertValue) : null);
+			const insertValue = newValue ? newValue.toString().replace(/[\D\.]/g, '') : newValue.toString().trim() === '0' ? 0 : null;
+			self.Start.TimeUnitValue(insertValue === null ? null : Number(insertValue));
+			self.Start.TimeUnit() === 'day' &&	self.Start.Days(insertValue === null ? null : Number(insertValue));
 		})
 
 		self.End.TimeUnitValue.subscribe(function (newValue){
-			const insertValue = newValue ? newValue.toString().replace(/[\D\.]/g, '') : null;
-			self.End.TimeUnitValue(insertValue ? Number(insertValue) : null);
-			self.End.TimeUnit() === 'day' && self.End.Days(insertValue ? Number(insertValue) : null);
+			const insertValue = newValue ? newValue.toString().replace(/[\D\.]/g, '') : newValue.toString().trim() === '0' ? 0 : null;
+			self.End.TimeUnitValue(insertValue === null ? null : Number(insertValue));
+			self.End.TimeUnit() === 'day' && self.End.Days(insertValue === null ? null : Number(insertValue));
 		})
 
 		self.Start.TimeUnit.subscribe(function (newValue){
