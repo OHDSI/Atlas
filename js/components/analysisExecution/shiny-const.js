0215a6ef7a28515294cf674bbfa97aeaad618b05
Oleg Kuzik <oleg_kuzik@epam.com>
2024-06-25 23:48:51 +0200

    [ATL-50] Introduced Cohort Pathway Shiny app download and publishing

--- js/components/analysisExecution/shiny-const.js
+++ js/components/analysisExecution/shiny-const.js
@@ -1,8 +1,10 @@
 define(
     (require, exports) => {
         const apiPaths = {
-            downloadShiny: (id, sourceKey) => `shiny/download/cohort_characterization/${id}/${sourceKey}`,
-            publishShiny: (id, sourceKey) => `shiny/publish/cohort_characterization/${id}/${sourceKey}`,
+            downloadShinyCC: (id, sourceKey) => `shiny/download/cohort_characterization/${id}/${sourceKey}`,
+            publishShinyCC: (id, sourceKey) => `shiny/publish/cohort_characterization/${id}/${sourceKey}`,
+            downloadShinyPW: (id, sourceKey) => `shiny/download/cohort_pathway/${id}/${sourceKey}`,
+            publishShinyPW: (id, sourceKey) => `shiny/publish/cohort_pathway/${id}/${sourceKey}`,
         };
         return {
             apiPaths
